7fd580eecdb020fc7148022ec78b73b8f2cd355e
takeshi8989 <htakeshi0614@gmail.com>
2023-11-13 13:33:55 -0800

    created simple header

--- matchya-web/src/components/Header.tsx
+++ matchya-web/src/components/Header.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import matchyaIcon from '../assets/matchya-icon.png'
 
 const Header = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+    const login = () => {
+        setIsAuthenticated(!isAuthenticated)
+    }
+
   return (
-    <div>Header</div>
+    <div className='w-full h-16 border-2 flex justify-between'>
+        <div className='w-1/5 flex justify-center items-center'>
+            <img className="h-3/4 rounded-full m-6" src={ matchyaIcon } />
+            <h3 className='text-lg m-6'>Matchya</h3>
+        </div>
+        <div className='w-1/4 flex justify-end items-center'>
+            {
+                isAuthenticated && (
+                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-6'>
+                        Settings
+                    </button>
+                )
+            }
+            <button 
+                className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-6'
+                onClick={login}>
+                { isAuthenticated ? 'Logout' : 'Login'}
+            </button>
+        </div>
+    </div>
   )
 }
 
