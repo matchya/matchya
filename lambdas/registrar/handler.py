194b95d275e43c105734e5c16f2f1b199f400313
takeshi8989 <htakeshi0614@gmail.com>
2023-11-14 22:12:38 -0800

    structured api code

--- lambdas/registrar/handler.py
+++ lambdas/registrar/handler.py
@@ -1,12 +1,33 @@
 import json
+import datetime
+
+# # serverless invoke local --function hello --data '{"body": {"company_name": "matchya", "email": "mathcyateam@gmail.com", "github_account_url": "https://github.com/takeshi8989"}}'
 
 
-def hello(event, context):
+# # define the DynamoDB table that Lambda will connect to
+# tableName = "lambda-apigateway"
+
+# # create the DynamoDB resource
+# dynamo = boto3.resource('dynamodb').Table(tableName)
+
+def register(event, context):
+
+    company_name = event['body']['company_name']
+    email = event['body']['email']
+    github_account_url = event['body']['github_account_url']
+    created_at = str(datetime.datetime.now())
+
+    # save to dynamodb
+    # dynamo.save(company_name, email, github_account_url, created_at)
+    # dynamo.put_item()
+
+    company_id = 1  # get from dynamodb
+
     body = {
-        "message": "Registrar endpoint! Your function executed successfully!",
-        "input": event,
+        "company_id": company_id,
+        "created_at": created_at
     }
 
-    response = {"statusCode": 200, "body": json.dumps(body)}
+    response = {"status": 200, "payload": json.dumps(body)}
 
     return response
