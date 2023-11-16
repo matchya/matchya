8c334faf90f8a8b150dcc81e91aa8aae644eae9f
kokoaj <kokiebisu@icloud.com>
2023-11-15 23:43:53 -0700

    feat: set up storybook (#21)
    * storybook setup
    
    * create stories and adjust structure
    
    * move assets into /public
    
    * fix lint-staged path from matchya-web to web

---
+++ web/src/screens/Settings/Settings.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import Settings from './Settings';
+
+export default {
+  title: 'Components/Settings',
+  component: Settings,
+} as Meta;
+
+export const Default: StoryObj<typeof Settings> = {};
