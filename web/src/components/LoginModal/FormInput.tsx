aa3f2be749f863634acb17dd2dfc84b16990d988
TAKESHI <109770597+takeshi8989@users.noreply.github.com>
2023-11-17 18:27:19 -0800

    6 feat web UI basic design (#7)
    * componentize setting inputs
    
    * componentize auth modal
    
    * updated header
    
    * deleted .vite folder
    
    * set up title and favicon
    
    * updated home screen
    
    * added gitignore
    
    * sort import
    
    * rename props
    
    * add types to useStates
    
    * remove extra spaces
    
    * set default props for custom button
    
    * added scorecard props
    
    * edit mock data and type
    
    ---------
    
    Co-authored-by: kokiebisu <kokiebisu@icloud.com>

---
+++ web/src/components/LoginModal/FormInput.tsx
@@ -0,0 +1,39 @@
+interface FormInputProps {
+  label?: string;
+  placeholder?: string;
+  id: string;
+  type: string;
+  className?: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const FormInput = ({
+  label,
+  placeholder,
+  id,
+  type,
+  className,
+  value,
+  onChange,
+}: FormInputProps) => {
+  return (
+    <div className={className}>
+      {label && (
+        <label htmlFor={id} className="block text-sm font-medium text-gray-700">
+          {label}:
+        </label>
+      )}
+      <input
+        type={type}
+        id={id}
+        placeholder={placeholder}
+        className="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-lime-300 focus:border-lime-300"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default FormInput;
