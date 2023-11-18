35ba76c5660349ba1ebfb069a6e6502ef1ea5db3
kokiebisu <kokiebisu@icloud.com>
2023-11-18 15:54:17 -0700

    update register logic

--- lambdas/authentication/handler.py
+++ lambdas/authentication/handler.py
@@ -1,21 +1,56 @@
 import json
 import datetime
+import os
+import uuid
 
 import boto3
 
-table_name = "CompanyInfo"
-dynamo = boto3.resource('dynamodb').Table(table_name)
+environment = os.environ.get('ENVIRONMENT')
+
+dynamodb = boto3.resource('dynamodb')
+company_table = dynamodb.Table(f'{environment}-Company')
+access_token_table = dynamodb.Table(f'{environment}-AccessToken')
 
 
 def register(event, context):
+    # Parse the JSON body from the event
+    try:
+        body = event.get('body', '')
+        if not body:
+            raise json.JSONDecodeError
+        body = json.loads(event.get('body', ''))
+    except json.JSONDecodeError:
+        return {'statusCode': 400, 'body': 'Invalid JSON in request body'}
+
+    # Validate that required fields are present
+    email = body.get('email')
+    company_name = body.get('company_name')
+    github_account_url = body.get('github_account_url')
+    password = body.get('password')
+
+    if not all([email, company_name, github_account_url, password]):
+        return {'statusCode': 400, 'body': 'Missing required fields'}
 
     body = json.loads(event['body'])
-    company_id = '1'
-    # TODO: retrieve these fields from the db
-    # company_name = body['company_name']
-    # email = body['email']
-    # github_account_url = body['github_account_url']
+
     created_at = str(datetime.datetime.now())
+
+    # Generate a unique companyId
+    company_id = str(uuid.uuid4())
+
+    company_info = {
+        'company_id': company_id,
+        'company_name': company_name,
+        'email': email,
+        'github_account_url': github_account_url,
+        'password': event.get('password')  # Ensure this is securely hashed
+    }
+
+    try:
+        company_table.put_item(Item=company_info)
+    except Exception:
+        return {'statusCode': 400, 'body': 'Something went wrong while saving to db'}
+
     access_token = _generate_access_token(company_id)
 
     body = {
