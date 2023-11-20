279056d54aa11ed153cfda1fd7338add387f0d2e
takeshi8989 <htakeshi0614@gmail.com>
2023-11-20 00:16:32 -0800

    rename variables and methods

--- lambdas/generator/handler.py
+++ lambdas/generator/handler.py
@@ -24,6 +24,9 @@ criteria_table = dynamodb.Table(f'{environment}-Criteria')
 # OpenAI API
 chat_client = OpenAI()
 
+# GitHub API Headers
+github_api_headers = {'Authorization-Type': "Bearer " + os.environ['GITHUB_TOKEN'], "X-GitHub-Api-Version": "2022-11-28"}
+
 
 def generate_criteria(event, context):
 
@@ -37,53 +40,33 @@ def generate_criteria(event, context):
 
 
     # Validate that required fields are present
-    company_id = body.get('company_id')
-    position = body.get('position', 'default')
+    position_id = body.get('position_id', '')
+
+    # TODO:Get github_username and repo names from database by position_id
+    github_username = "kokiebisu"
+    repo_names = ["rental-storage"]
+    repository_name = repo_names[0]     # TODO: For loop
 
 
-    # get company github url from db
-    try:
-        response = dynamodb_client.get_item(
-            TableName=f'{environment}-Company',
-            Key={'company_id': {'S': company_id}},
-        )
-        if 'Item' not in response:
-            return {'statusCode': 400, 'body': 'Company does not exist'}
-        github_account_url = response['Item']['github_account_url']
-        print("github_account_url from db: ", github_account_url)
-
-    except Exception:
-        return {'statusCode': 400, 'body': 'Something went wrong while getting company github url'}
-    
-    github_repo_url = "https://github.com/kokiebisu/rental-storage"  # mock
-
-    github_user_name = github_repo_url.split('/')[-2]
-    repository_name = github_repo_url.split('/')[-1]
-
-    languages = get_programming_languages_used(github_user_name, repository_name)
-
-    important_file_names = get_important_file_names(github_user_name, repository_name, languages)
+    programming_languages = get_programming_languages_used(github_username, repository_name)
+    important_file_names = get_important_file_names(programming_languages)
 
     prompt = "Please review the following files.\n"
-    file_paths = get_important_file_urls(github_user_name, repository_name, important_file_names)
+    file_paths = get_important_file_paths(github_username, repository_name, important_file_names)
 
     for file_path in file_paths:
-        prompt = prompt + file_path + ":\n" + get_file_contents(github_user_name, repository_name, file_path) + "\n"
+        prompt = prompt + file_path + ":\n" + get_file_content(github_username, repository_name, file_path) + "\n"
 
-    tech_stack = get_criteria(prompt, languages)
-    print("tech_stack: ", tech_stack)
+    criteria_keywords = get_criteria_keywords(prompt, programming_languages)
+    print("criteria: ", criteria_keywords)
 
-    # save criteria to db
-    criteria_id = str(uuid.uuid4())
+    criterion_id = str(uuid.uuid4())
     created_at = str(datetime.datetime.now())
 
-    try:
-        store_criteria_in_db(criteria_id, company_id, position, github_repo_url, tech_stack, created_at)
-    except Exception:
-        return {'statusCode': 400, 'body': 'Something went wrong while saving to criteria table'}
+    #TODO: Save criteria to database
 
     body = {
-        "critria_id": criteria_id,
+        "critrion_id": criterion_id,
         "created_at": created_at,
     }
 
@@ -94,7 +77,7 @@ def generate_criteria(event, context):
 
 def get_programming_languages_used(github_username, repository_name):
     url = "https://api.github.com/repos/" + github_username + "/" + repository_name + "/languages"
-    res = requests.get(url, headers={'Authorization-Type': "Bearer " + os.environ['GITHUB_TOKEN'], "X-GitHub-Api-Version": "2022-11-28"})
+    res = requests.get(url, headers=github_api_headers)
 
     data = json.loads(res.content)
     languages = []
@@ -103,7 +86,7 @@ def get_programming_languages_used(github_username, repository_name):
     return languages
 
 
-def get_important_file_names(github_username, repository_name, languages):
+def get_important_file_names(languages):
     important_file_names = ["README.md"]
 
     package_file_names = {
@@ -130,24 +113,11 @@ def get_important_file_names(github_username, repository_name, languages):
     return important_file_names
 
 
-def get_repository_names(github_username):
-    # GitHub API
-    url = "https://api.github.com/users/" + github_username + "/repos"
-    res = requests.get(url, headers={'Authorization-Type': "Bearer " + os.environ['GITHUB_TOKEN'], "X-GitHub-Api-Version": "2022-11-28"})
+def get_important_file_paths(github_username, repo_name, important_file_names):
+    branch = get_default_branch(github_username, repo_name)
 
-    data = json.loads(res.content)
-    repository_urls = []
-    for repo in data:
-        repository_urls.append(repo['name'])
-    return repository_urls
-
-
-def get_important_file_urls(user_name, repo_name, important_file_names):
-    # GitHub API
-    branch = get_default_branch(user_name, repo_name)
-
-    url = "https://api.github.com/repos/" + user_name + "/" + repo_name + "/git/trees/" + branch + "?recursive=1"
-    res = requests.get(url, headers={'Authorization-Type': "Bearer " + os.environ['GITHUB_TOKEN'], "X-GitHub-Api-Version": "2022-11-28"})
+    url = "https://api.github.com/repos/" + github_username + "/" + repo_name + "/git/trees/" + branch + "?recursive=1"
+    res = requests.get(url, headers=github_api_headers)
 
     data = json.loads(res.content)
     tree = data['tree']
@@ -158,26 +128,25 @@ def get_important_file_urls(user_name, repo_name, important_file_names):
     return file_paths
 
 
-def get_default_branch(user_name, repo_name):
-    # GitHub API
-    url = "https://api.github.com/repos/" + user_name + "/" + repo_name
-    res = requests.get(url, headers={'Authorization-Type': "Bearer " + os.environ['GITHUB_TOKEN'], "X-GitHub-Api-Version": "2022-11-28"})
+def get_default_branch(github_username, repo_name):
+    url = "https://api.github.com/repos/" + github_username + "/" + repo_name
+    res = requests.get(url, headers=github_api_headers)
 
     data = json.loads(res.content)
     return data['default_branch']
 
 
 def is_important_file(path, important_file_names):
-    # get files with names in important_file_names, and only contains ~2 / (not in a subdirectory)
+    # get files with names in important_file_names, and only contains ~ 1 / (not in a subdirectory)
     for file_name in important_file_names:
         if file_name in path and path.count('/') <= 1:
             return True
     return False
 
 
-def get_file_contents(github_user_name, repository_name, file_path):
-    url = "https://api.github.com/repos/" + github_user_name + "/" + repository_name + "/contents/" + file_path
-    res = requests.get(url, headers={'Authorization-Type': "Bearer " + os.environ['GITHUB_TOKEN'], "X-GitHub-Api-Version": "2022-11-28"})
+def get_file_content(github_username, repository_name, file_path):
+    url = "https://api.github.com/repos/" + github_username + "/" + repository_name + "/contents/" + file_path
+    res = requests.get(url, headers=github_api_headers)
 
     data = json.loads(res.content)
     content_encoded = data['content']
@@ -185,7 +154,7 @@ def get_file_contents(github_user_name, repository_name, file_path):
     return content
 
 
-def get_criteria(prompt, languages):
+def get_criteria_keywords(prompt, languages):
     system_message = "You read the following files. Please review them and generate a list of programming languages, libraries, and other technologies used in the project. You should always put these languages first in the head of the list in order." + "Languages:"
     for language in languages:
         system_message = system_message + language['name'] + ", "
@@ -199,18 +168,5 @@ def get_criteria(prompt, languages):
         ]
     )
     content = json.loads(completion.choices[0].message.content)
-    criteria = content['criteria']
-    return criteria
-
-
-def store_criteria_in_db(criteria_id, company_id, position, github_repo_url, tech_stack, created_at):
-    criteria_info = {
-        'criteria_id': criteria_id,
-        'company_id': company_id,
-        'position': position,
-        'repository_url': github_repo_url,
-        'tech_stack': set(tech_stack),
-        'created_at': created_at
-    }
-
-    criteria_table.put_item(Item=criteria_info)
+    criteria_keywords = content['criteria']
+    return criteria_keywords
