ccd56e4ffa143cbf68b0bad7d092ec6b815254fa
takeshi8989 <htakeshi0614@gmail.com>
2023-11-19 23:59:22 -0800

    renamed variables

--- lambdas/criteria_retriever/handler.py
+++ lambdas/criteria_retriever/handler.py
@@ -1,5 +1,4 @@
 import json
-import boto3
 import os
 from os.path import join, dirname
 
@@ -8,36 +7,19 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-environment = os.environ.get('ENVIRONMENT')
-dynamodb = boto3.resource('dynamodb')
-dynamodb_client = boto3.client('dynamodb')
-
 
 def retrieve_criteria(event, context):
     parameter = event.get('pathParameters')
     if parameter is None:
         return {'statusCode': 400, 'body': 'Missing required parameters'}
-    criteria_id = parameter.get('id')
+    position_id = parameter.get('id')
     
-    if criteria_id is None:
-        return {'statusCode': 400, 'body': 'Criteria id is required'}
+    if position_id is None:
+        return {'statusCode': 400, 'body': 'Position id is required'}
 
-    criteria = None
-    try:
-        response = dynamodb_client.get_item(
-            TableName=f'{environment}-Criteria',
-            Key={'criteria_id': {'S': criteria_id}},
-        )
-        if 'Item' not in response:
-            return {'statusCode': 400, 'body': 'Criteria does not exist'}
-        criteria = response['Item']['tech_stack']
-    except Exception:
-        return {'statusCode': 400, 'body': 'Something went wrong while getting company github url'}
-
-    if criteria is None:
-        return {'statusCode': 400, 'body': 'Something went wrong while getting company github url'}
+    # TODO: Get Criteria Full Messages by position id from Database
     
-    criteria = criteria['SS']
+    criteria = ["Criteria 1", "Criteria 2", "Criteria 3"]   # mock
     body = {
         "criteria": criteria,
     }
