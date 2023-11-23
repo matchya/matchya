c7b6248a167c1efddfdab36e350108590f374425
takeshi8989 <htakeshi0614@gmail.com>
2023-11-22 22:19:06 -0800

    request post method to generator lambda

--- web/src/config/env.ts
+++ web/src/config/env.ts
@@ -1,1 +1,3 @@
 export const apiEndpoint = import.meta.env.VITE_API_URL;
+export const authEndpoint = import.meta.env.VITE_AUTH_URL;
+export const criteriaEndpoint = import.meta.env.VITE_CRITERIA_URL;
---
+++ web/src/screens/Dashboard/CriteriaBox.tsx
@@ -0,0 +1,92 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+import Button from '../../components/LoginModal/Button';
+import FormInput from '../../components/LoginModal/FormInput';
+import { criteriaEndpoint } from '../../config';
+import { mockCriteria } from '../../data';
+import { useAuthStore } from '../../store/useAuthStore';
+
+const CriteriaBox = () => {
+
+    const [criteriaGenerated, setCriteriaGenerated] = useState<boolean>(false);
+    const [inputRepository, setInputRepository] = useState<string>('');
+    const [repositoryNames, setRepositoryNames] = useState<string[]>([]);
+    const { accessToken } = useAuthStore();
+
+    const generateCriteria = async () => {
+        const userData = {"position_id": "1", "repo_names": repositoryNames};
+        try {
+            const response = await axios.post(
+                `${criteriaEndpoint}/generate`,
+                userData,
+                {headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${accessToken}`}}
+            );
+            console.log(response.data)
+            if (response.data.status == 'success') {
+              // do something
+              setCriteriaGenerated(true);
+            }
+        } catch (error) {
+            console.error('Generating Criteria failed:', error);
+            // Handle error (e.g., show error message to the user)
+        }
+    }
+  
+    if (!criteriaGenerated) {
+      return (
+        <div className="px-6 py-4 h-full flex flex-col justify-center items-center">
+          <h3 className="text-lg font-bold">Generate Criteria</h3>
+          <p className="text-sm text-gray-600 mt-4">
+            Generate criteria to get started
+          </p>
+          <FormInput
+            label="Repository Names To Generate Criteria For"
+            id="repo_names"
+            type="text"
+            className="mt-4"
+            value={inputRepository}
+            onChange={e => setInputRepository(e.target.value)}
+          />
+          <Button
+            text="Add"
+            color="green"
+            className="mt-4"
+            onClick={() => setRepositoryNames([...repositoryNames, inputRepository])}
+          />
+          {repositoryNames.map((repo, index) => (
+            <div key={index} className="flex justify-between items-center w-full mt-4">
+              <p className="text-sm text-gray-600">{repo}</p>
+              <Button
+                text="Remove"
+                color="red"
+                className="w-1/3"
+                onClick={() => setRepositoryNames(repositoryNames.filter(r => r !== repo))}
+              />
+            </div>
+          ))}
+          <Button
+            text="Generate"
+            color="green"
+            className="mt-4"
+            onClick={generateCriteria}
+          />
+        </div>
+      );
+    }
+  
+    return (
+      <div className="px-6 py-4 h-full">
+        <h3 className="text-lg font-bold">Generated Criteria</h3>
+        <ul className="list-disc pl-6 mt-4">
+          {mockCriteria.map((criterion, index) => (
+            <li key={index} className="text-sm text-gray-600">
+              {criterion}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
+export default CriteriaBox;
