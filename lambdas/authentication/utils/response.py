283aee1f5b89f29ad2598b17eb9920f5e1fb578f
takeshi8989 <htakeshi0614@gmail.com>
2023-11-23 12:15:58 -0800

    change the header in authentication lambda

--- lambdas/authentication/utils/response.py
+++ lambdas/authentication/utils/response.py
@@ -4,10 +4,9 @@ from typing import Any, Dict
 
 
 COMMON_HEADERS = {
-    'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Credentials': True,
+    'Access-Control-Allow-Methods': 'OPTIONS,POST',
 }
 
 
