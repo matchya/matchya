ae276cf309914c0eab7cdcf4c00fbbcfeca363bf
kokiebisu <kokiebisu@icloud.com>
2023-11-23 17:43:56 -0700

    set up iam module in shared

---
+++ terraform/shared/modules/iam/ssm.tf
@@ -0,0 +1,6 @@
+resource "aws_ssm_parameter" "service-lambda_role_arn" {
+  count = var.create_new ? 1 : 0
+  name  = "/terraform/iam/lambda-role-arn"
+  type  = "String"
+  value = aws_iam_role.lambda_role.arn
+}
