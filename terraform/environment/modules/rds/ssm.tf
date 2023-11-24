a67465d4c421c5c30e6bd61fa04bd9e8c7b34f7f
kokiebisu <kokiebisu@icloud.com>
2023-11-23 19:04:07 -0700

    readd rds ssm params

--- terraform/environment/modules/rds/ssm.tf
+++ terraform/environment/modules/rds/ssm.tf
@@ -3,3 +3,27 @@ resource "aws_ssm_parameter" "db_instance_endpoint" {
   type  = "String"
   value = aws_db_instance.this.endpoint
 }
+
+resource "aws_ssm_parameter" "db_instance_name" {
+  name  = "/terraform/${terraform.workspace}/rds/db_name"
+  type  = "String"
+  value = aws_db_instance.this.db_name
+}
+
+resource "aws_ssm_parameter" "db_instance_port" {
+  name  = "/terraform/${terraform.workspace}/rds/port"
+  type  = "String"
+  value = tostring(aws_db_instance.this.port)
+}
+
+resource "aws_ssm_parameter" "db_instance_password" {
+  name  = "/terraform/${terraform.workspace}/rds/db_password"
+  type  = "SecureString"
+  value = var.db_password
+}
+
+resource "aws_ssm_parameter" "db_instance_username" {
+  name  = "/terraform/${terraform.workspace}/rds/db_username"
+  type  = "String"
+  value = var.db_username
+}
