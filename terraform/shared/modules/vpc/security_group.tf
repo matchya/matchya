3ae16fb940049a30d754fe4b79ff78a20b4d54d8
kokiebisu <kokiebisu@icloud.com>
2023-11-25 12:45:26 -0700

    set up lambda security group

--- terraform/shared/modules/vpc/security_group.tf
+++ terraform/shared/modules/vpc/security_group.tf
@@ -1,3 +1,54 @@
+resource "aws_security_group" "lambda" {
+  count      = var.create_new ? 1 : 0
+  description = "Security group for Serverless functions"
+  vpc_id      = aws_vpc.main[0].id
+
+  ingress {
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 6379
+    to_port          = 6379
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  tags = {
+    Name = "lambda"
+  }
+}
+
 resource "aws_security_group" "rds" {
   count      = var.create_new ? 1 : 0
   name        = "rds-security-group"
