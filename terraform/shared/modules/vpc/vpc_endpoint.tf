67d378cdd41a6505a6835d99579f640e85dd3b7c
kokiebisu <kokiebisu@icloud.com>
2023-11-25 12:48:44 -0700

    set up vpc endpoint

---
+++ terraform/shared/modules/vpc/vpc_endpoint.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc_endpoint" "rds" {
+  count      = 0 // save money, only switch when using
+  vpc_id              = aws_vpc.main[0].id
+  service_name        = "com.amazonaws.us-east-1.rds"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+
+  security_group_ids = [aws_security_group.vpc_endpoint[0].id]
+  subnet_ids         = [aws_subnet.private_1[0].id, aws_subnet.private_2[0].id]
+
+  tags = {
+    Name = "rds"
+  }
+}
