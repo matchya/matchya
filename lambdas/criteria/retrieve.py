f33a56282eb44ed1722f71b2556108904a3f31d1
takeshi8989 <htakeshi0614@gmail.com>
2023-11-24 23:17:55 -0800

    retrieve criteria from dynamo

--- lambdas/criteria/retrieve.py
+++ lambdas/criteria/retrieve.py
@@ -1,21 +1,45 @@
-import json
+import boto3
+
+from config import Config
 
 from utils.response import generate_response, generate_success_response
+from utils.request import parse_request_parameter
+
+dynamodb = boto3.resource('dynamodb')
+dynamodb_client = boto3.client('dynamodb')
+CRITERION_TABLE_NAME = f'{Config.ENVIRONMENT}-Criterion'
+criterion_table = dynamodb.Table(CRITERION_TABLE_NAME)
+
 
 def handler(event, context):
-    parameter = event.get('pathParameters')
-    if parameter is None:
-        return generate_response(400, json.dumps({"message": "Missing required parameters"}))
-    position_id = parameter.get('id')
+    try:
+        position_id = parse_request_parameter(event, 'id')
+        criteria = get_criteria_by_position_id(position_id)
+        body = {
+            "criteria": criteria,
+        }
+        return generate_success_response(body)
+    except Exception as e:
+        print(e)
+        return generate_response(500, {"message": f"Failed to retrieve criteria: {e}"})
 
-    if position_id is None:
-        return generate_response(400, json.dumps({"message": "Missing required parameters"}))
 
-    # TODO: Get Criteria Full Messages by position id from Database
-
-    criteria = ["Criteria 1", "Criteria 2", "Criteria 3"]   # mock
-    body = {
-        "criteria": criteria,
-    }
-
-    return generate_success_response(body)
+def get_criteria_by_position_id(position_id):
+    """
+    Retrieves the 'message' attribute of criteria for a given position_id from the database.
+    
+    :param position_id: Unique identifier for the position.
+    :return: List of messages for the given position_id.
+    """
+    try:
+        response = criterion_table.query(
+            IndexName='position-id-index',
+            KeyConditionExpression=boto3.dynamodb.conditions.Key('position_id').eq(position_id),
+            ProjectionExpression='message'
+        )
+        messages = [item['message'] for item in response.get('Items', [])]
+        if not messages:
+            raise ValueError(f"Criteria not found for position_id: {position_id}")
+        return messages
+    except Exception as e:
+        raise RuntimeError(f"Failed to retrieve criteria messages: {e}")
