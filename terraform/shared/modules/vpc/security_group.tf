81886ed1abdb85af155333568233dea889895003
kokiebisu <kokiebisu@icloud.com>
2023-11-25 12:46:14 -0700

    set up new ec2 public security group

--- terraform/shared/modules/vpc/security_group.tf
+++ terraform/shared/modules/vpc/security_group.tf
@@ -107,6 +107,57 @@ resource "aws_security_group" "rds_postgres" {
   }
 }
 
+resource "aws_security_group" "ec2_public" {
+  count      = var.create_new ? 1 : 0
+  name = "ec2-public"
+  description = "Security group for EC2 Public Bastion Host. Used to access private resources within VPC."
+  vpc_id      = aws_vpc.main[0].id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 6379
+    to_port          = 6379
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    "Name" = "ec2-public"
+  }
+}
+
 data "aws_security_group" "rds" {
   count      = var.create_new ? 0 : 1
   name = "rds-security-group"
