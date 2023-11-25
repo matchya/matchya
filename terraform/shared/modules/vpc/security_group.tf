1d2a1c000a49ec7bfda69a8354a25cfbee5d02a4
kokoaj <kokiebisu@icloud.com>
2023-11-25 12:50:40 -0700

    feat(infra): set up basic infrastructure for new vpc (#138)
    * set up new vpc
    
    * set up subnets
    
    * set up lambda security group
    
    * set up new rds security group
    
    * set up new ec2 public security group
    
    * set up vpc endpoint security group
    
    * set up data for rds, ec2 public security groups
    
    * set up vpc endpoint
    
    * set up internet gateway
    
    * set up outputs to expose the vpc resources to environment modules

--- terraform/shared/modules/vpc/security_group.tf
+++ terraform/shared/modules/vpc/security_group.tf
@@ -1,3 +1,54 @@
+resource "aws_security_group" "lambda" {
+  count      = var.create_new ? 1 : 0
+  description = "Security group for Serverless functions"
+  vpc_id      = aws_vpc.main[0].id
+
+  ingress {
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 6379
+    to_port          = 6379
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  tags = {
+    Name = "lambda"
+  }
+}
+
 resource "aws_security_group" "rds" {
   count      = var.create_new ? 1 : 0
   name        = "rds-security-group"
@@ -23,7 +74,126 @@ resource "aws_security_group" "rds" {
   }
 }
 
+# testing new one inside vpc
+resource "aws_security_group" "rds_postgres" {
+  count      = var.create_new ? 1 : 0
+  name        = "rds-security-group-new"
+  description = "Security group for RDS Postgres database"
+  vpc_id      = aws_vpc.main[0].id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-postgres-new"
+  }
+}
+
+resource "aws_security_group" "ec2_public" {
+  count      = var.create_new ? 1 : 0
+  name = "ec2-public"
+  description = "Security group for EC2 Public Bastion Host. Used to access private resources within VPC."
+  vpc_id      = aws_vpc.main[0].id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 6379
+    to_port          = 6379
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    "Name" = "ec2-public"
+  }
+}
+
+resource "aws_security_group" "vpc_endpoint" {
+  count      = var.create_new ? 1 : 0
+  description = "Security group for VPC Endpoint"
+  vpc_id      = aws_vpc.main[0].id
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = -1
+    security_groups = [aws_security_group.lambda[0].id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name" = "vpc-endpoint"
+  }
+}
+
+# DATA
 data "aws_security_group" "rds" {
   count      = var.create_new ? 0 : 1
   name = "rds-security-group"
+}
+
+data "aws_security_group" "rds_postgres" {
+  count      = var.create_new ? 0 : 1
+  name = "rds-security-group-new"
+}
+
+data "aws_security_group" "ec2_public" {
+  count      = var.create_new ? 0 : 1
+  name = "ec2-public"
 }
