6561d88f7b5612f0c5eebefb7f1177bedaaeb9ad
takeshi8989 <htakeshi0614@gmail.com>
2023-11-24 23:54:50 -0800

    add candidate table

--- db/changelog/01-create-tables.sql
+++ db/changelog/01-create-tables.sql
@@ -1,3 +1,6 @@
+--liquibase formatted sql
+
+--changeset author:1
 CREATE TABLE IF NOT EXISTS company (
 	id varchar(255) NOT NULL PRIMARY KEY,
 	name varchar(255) NOT NULL,
@@ -6,7 +9,9 @@ CREATE TABLE IF NOT EXISTS company (
 	password bytea,
 	created_at timestamp DEFAULT CURRENT_TIMESTAMP
 );
+--rollback DROP TABLE IF EXISTS company;
 
+--changeset author:2
 CREATE TABLE IF NOT EXISTS position (
 	id varchar(255) not null primary key,
 	company_id varchar(255),
@@ -14,3 +19,15 @@ CREATE TABLE IF NOT EXISTS position (
 	created_at timestamp default current_timestamp,
 	foreign key (company_id) references company(id)
 );
+--rollback DROP TABLE IF EXISTS position;
+
+--changeset author:3
+CREATE TABLE IF NOT EXISTS candidate (
+	id varchar(255) not null primary key,
+	first_name varchar(255),
+	last_name varchar(255),
+	github_username varchar(255),
+	email varchar(255) unique,
+	created_at timestamp default current_timestamp
+);
+--rollback DROP TABLE IF EXISTS candidate;
