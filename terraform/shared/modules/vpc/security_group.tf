bc9ad763ba8cea6eb2a190608e09463dc36d63c6
kokiebisu <kokiebisu@icloud.com>
2023-11-25 12:45:54 -0700

    set up new rds security group

--- terraform/shared/modules/vpc/security_group.tf
+++ terraform/shared/modules/vpc/security_group.tf
@@ -74,6 +74,39 @@ resource "aws_security_group" "rds" {
   }
 }
 
+# testing new one inside vpc
+resource "aws_security_group" "rds_postgres" {
+  count      = var.create_new ? 1 : 0
+  name        = "rds-security-group-new"
+  description = "Security group for RDS Postgres database"
+  vpc_id      = aws_vpc.main[0].id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-postgres-new"
+  }
+}
+
 data "aws_security_group" "rds" {
   count      = var.create_new ? 0 : 1
   name = "rds-security-group"
