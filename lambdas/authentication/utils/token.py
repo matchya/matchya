8a3c14250e3571bdbdfb3cf57e297abcc25d643a
kokoaj <kokiebisu@icloud.com>
2023-11-25 20:46:28 -0700

    feat: make register attach cookie instead of response body (#156)
    * set up JWT_SECRET_KEY in config
    
    * attach cookie on success
    
    * add logic to generate access token
    
    * fix lint

--- lambdas/authentication/utils/token.py
+++ lambdas/authentication/utils/token.py
@@ -1,2 +1,10 @@
+import jwt
+
+from config import Config
+
+
 def generate_access_token(company_id):
-    return f'{company_id}_access_token'
+    payload = {
+        "company_id": company_id
+    }
+    return jwt.encode(payload, Config.JWT_SECRET_KEY, algorithm="HS256")
