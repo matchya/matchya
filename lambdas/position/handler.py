87378332208667ce65ec976d1c4712ae84ac24a8
takeshi8989 <htakeshi0614@gmail.com>
2023-11-25 20:34:38 -0800

    copy util files

--- lambdas/position/handler.py
+++ lambdas/position/handler.py
@@ -1,5 +1,30 @@
 import json
 
+# response_body {
+# 	id: string
+# 	name: string
+# 	criteria: {
+# 		repository_names: string[]
+# 		messages: string[]        // messages for each criterion
+# 	}
+# 	candidates: [
+# 		{
+# 			first_name: string,
+# 			last_name: string,
+# 			email: string,
+# 			github_username: string,
+# 			total_score: number,
+# 			summary: string,
+# 			assessments: [
+# 				{
+# 					criterion: string
+# 					score: number
+# 					reason: string
+# 				},
+# 			]
+# 		},
+# 	]
+# }
 
 def retrieve(event, context):
     body = {
