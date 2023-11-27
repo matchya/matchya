58f9bc072de10018bd959a57c514a2be8ef52302
takeshi8989 <htakeshi0614@gmail.com>
2023-11-26 23:24:33 -0800

    fix

--- lambdas/criteria/retrieve.py
+++ lambdas/criteria/retrieve.py
@@ -1,9 +1,20 @@
+import logging
+
 import boto3
 
 from config import Config
 
-from utils.response import generate_response, generate_success_response
+from utils.response import generate_success_response, generate_error_response
 from utils.request import parse_request_parameter
+
+# Logger
+logger = logging.getLogger('retrieve criteria')
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 dynamodb = boto3.resource('dynamodb')
 dynamodb_client = boto3.client('dynamodb')
@@ -13,15 +24,20 @@ criterion_table = dynamodb.Table(CRITERION_TABLE_NAME)
 
 def handler(event, context):
     try:
+        logger.info(f"Received event: {event}")
         position_id = parse_request_parameter(event, 'id')
         criteria = get_criteria_by_position_id(position_id)
         body = {
             "criteria": criteria,
         }
+        logger.info(f"Successfully retrieved criteria")
         return generate_success_response(body)
+    except (ValueError, RuntimeError) as e:
+        logger.error(f"Failed to retrieve criteria: {e}")
+        return generate_error_response(400, f"Failed to retrieve criteria: {e}")
     except Exception as e:
-        print(e)
-        return generate_response(500, {"message": f"Failed to retrieve criteria: {e}"})
+        logger.error(f"Failed to retrieve criteria: {e}")
+        return generate_error_response(500, f"Failed to retrieve criteria: {e}")
 
 
 def get_criteria_by_position_id(position_id):
