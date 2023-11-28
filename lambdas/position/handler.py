cd72130d9c21d95c0d54736bcef471cdda9801ac
takeshi8989 <htakeshi0614@gmail.com>
2023-11-27 22:59:10 -0800

    return empty array if no checklist has been created

--- lambdas/position/handler.py
+++ lambdas/position/handler.py
@@ -88,11 +88,79 @@ def get_position_details_by_id(position_id):
     try:
         db_cursor.execute(sql)
         results = db_cursor.fetchall()
-        if not results:
+        if not results or len(results) == 0:
             raise ValueError(f"Position not found for id: {position_id}")
         return process_position_from_sql_results(results)
     except Exception as e:
         raise RuntimeError(f"Failed to retrieve position: {e}")
+
+
+def process_position_from_sql_results(sql_results):
+    """
+    Processes sql results from db_cursor.execute(sql) and returns position_data
+    
+    :param sql_results: sql results from db_cursor.execute(sql)
+    :return: position_data
+    """
+    print("HERE, running process_position_from_sql_results")
+
+    # if no checklist, return empty checklist
+    if sql_results[0][2] is None:
+        return {"name": sql_results[0][1], "checklists": []}
+
+    position_data = {}
+    for row in sql_results:
+        (position_id, position_name, checklist_id, repo_name, first_name, last_name, email, 
+         github_username, candidate_result_id, total_score, summary, criterion_id, score, reason) = row
+
+        if position_id not in position_data:
+            position_data[position_id] = {'name': position_name, 'checklists': {}}
+
+        if checklist_id not in position_data[position_id]['checklists']:
+            criteria_dict = get_criteria_dict_by_checklist_id(checklist_id)
+            position_data[position_id]['checklists'][checklist_id] = {
+                'id': checklist_id, 
+                'repository_names': set(),
+                'candidates': {},
+                'criteria': criteria_dict  # {id: message}
+            }
+
+        position_data[position_id]['checklists'][checklist_id]['repository_names'].add(repo_name)
+
+        candidates = position_data[position_id]['checklists'][checklist_id]['candidates']
+        if email and email not in candidates:
+            candidates[email] = {
+                'first_name': first_name,
+                'last_name': last_name,
+                'email': email,
+                'github_username': github_username,
+                'total_score': total_score,
+                'summary': summary,
+                'assessments': []
+            }
+
+        if email:
+            candidates[email]['assessments'].append({
+                'criterion_id': criterion_id,
+                'score': score,
+                'reason': reason
+            })
+
+    print("HERE, get position data")
+
+    final_data = []
+    for pos_id, pos_info in position_data.items():
+        checklists = []
+        for chk_id, chk_info in pos_info['checklists'].items():
+            chk_info['repository_names'] = list(chk_info['repository_names'])
+            chk_info['candidates'] = list(chk_info['candidates'].values())
+            checklists.append(chk_info)
+        final_data.append({
+            'name': pos_info['name'],
+            'checklists': checklists
+        })
+
+    return final_data[0] if final_data else None
 
 
 def get_criteria_dict_by_checklist_id(checklist_id):
@@ -116,64 +184,3 @@ def get_criteria_dict_by_checklist_id(checklist_id):
         return criteria
     except Exception as e:
         raise RuntimeError(f"Failed to retrieve criteria messages: {e}")
-
-def process_position_from_sql_results(sql_results):
-    """
-    Processes sql results from db_cursor.execute(sql) and returns position_data
-    
-    :param sql_results: sql results from db_cursor.execute(sql)
-    :return: position_data
-    """
-    print("HERE, running process_position_from_sql_results")
-    position_data = {}
-    for row in sql_results:
-        (position_id, position_name, checklist_id, repo_name, first_name, last_name, email, 
-         github_username, candidate_result_id, total_score, summary, criterion_id, score, reason) = row
-
-        if position_id not in position_data:
-            position_data[position_id] = {'name': position_name, 'checklists': {}}
-
-        if checklist_id not in position_data[position_id]['checklists']:
-            criteria_dict = get_criteria_dict_by_checklist_id(checklist_id)
-            position_data[position_id]['checklists'][checklist_id] = {
-                'id': checklist_id, 
-                'repository_names': set(),
-                'candidates': {},
-                'criteria': criteria_dict  # {id: message}
-            }
-
-        position_data[position_id]['checklists'][checklist_id]['repository_names'].add(repo_name)
-
-        candidates = position_data[position_id]['checklists'][checklist_id]['candidates']
-        if email not in candidates:
-            candidates[email] = {
-                'first_name': first_name,
-                'last_name': last_name,
-                'email': email,
-                'github_username': github_username,
-                'total_score': total_score,
-                'summary': summary,
-                'assessments': []
-            }
-
-        candidates[email]['assessments'].append({
-            'criterion_id': criterion_id,
-            'score': score,
-            'reason': reason
-        })
-
-    print("HERE, get position data")
-
-    final_data = []
-    for pos_id, pos_info in position_data.items():
-        checklists = []
-        for chk_id, chk_info in pos_info['checklists'].items():
-            chk_info['repository_names'] = list(chk_info['repository_names'])
-            chk_info['candidates'] = list(chk_info['candidates'].values())
-            checklists.append(chk_info)
-        final_data.append({
-            'name': pos_info['name'],
-            'checklists': checklists
-        })
-
-    return final_data[0] if final_data else None
