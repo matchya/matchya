49d48a29655b45f594093fcdd4e61e41f2f5d370
kokoaj <kokiebisu@icloud.com>
2023-11-28 18:37:06 -0700

    feat(infra): set up test rds inside vpc (#177)
    * set up rds_security_group_new in outputs
    
    * set up variables.tf for rds module
    
    * set up new rds instance + db subnet group
    
    * hook up new rds instance

--- terraform/environment/main.tf
+++ terraform/environment/main.tf
@@ -25,7 +25,11 @@ module "rds" {
 
   db_username = var.db_username
   db_password = var.db_password
-  rds_security_group_id = module.vpc.rds_security_group_id
+  rds_security_group = module.vpc.rds_security_group
+  rds_security_group_new = module.vpc.rds_security_group_new
+
+  private_subnet_1 = module.vpc.private_subnet_1
+  private_subnet_2 = module.vpc.private_subnet_2
 }
 
 module "vpc" {
