4b9290e897a4fb8e070bc9deeafb700a3f95998c
kokiebisu <kokiebisu@icloud.com>
2023-11-28 23:22:10 -0700

    update vite config to run on 127.0.0.1 instead of localhost

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,8 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+  },
 });
