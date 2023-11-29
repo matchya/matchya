67e01815db38d6ce96b6f50485fe3199e3cdb3e1
kokiebisu <kokiebisu@icloud.com>
2023-11-28 18:33:52 -0700

    set up variables.tf for rds module

--- terraform/environment/modules/rds/variable.tf
+++ terraform/environment/modules/rds/variable.tf
@@ -6,6 +6,18 @@ variable "db_password" {
   type = string
 }
 
-variable "rds_security_group_id" {
-  type = string
+variable "rds_security_group" {
+  type = any
+}
+
+variable "rds_security_group_new" {
+  type = any
+}
+
+variable "private_subnet_1" {
+  type = any
+}
+
+variable "private_subnet_2" {
+  type = any
 }
