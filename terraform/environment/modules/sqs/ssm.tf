7bade477cc3413915eae9ffdb7d2521c335f26ca
kokiebisu <kokiebisu@icloud.com>
2023-11-29 09:13:10 -0700

    set up queue + arn for criteria-evaluate

--- terraform/environment/modules/sqs/ssm.tf
+++ terraform/environment/modules/sqs/ssm.tf
@@ -4,3 +4,8 @@ resource "aws_ssm_parameter" "criteria_generate_queue_arn" {
   value = aws_sqs_queue.criteria_generate_queue.arn
 }
 
+resource "aws_ssm_parameter" "criteria_evaluate_queue_arn" {
+  name  = "/terraform/${terraform.workspace}/sqs/criteria_evaluate_queue_arn"
+  type  = "String"
+  value = aws_sqs_queue.criteria_evaluate_queue.arn
+}
