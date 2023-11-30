85e55482d986c2ad10651a8e8435db178cfe78fe
kokiebisu <kokiebisu@icloud.com>
2023-11-29 17:18:02 -0700

    rename queues from criteria to checklist

--- terraform/environment/modules/sqs/main.tf
+++ terraform/environment/modules/sqs/main.tf
@@ -1,13 +1,13 @@
-resource "aws_sqs_queue" "criteria_generate_queue" {
-  name                      = "${terraform.workspace}-criteria-generate"
+resource "aws_sqs_queue" "checklist_generation_processor_queue" {
+  name                      = "${terraform.workspace}-checklist-generation-processor-queue"
   delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
 }
 
-resource "aws_sqs_queue" "criteria_evaluate_queue" {
-  name                      = "${terraform.workspace}-criteria-evaluate"
+resource "aws_sqs_queue" "checklist_evaluation_processor_queue" {
+  name                      = "${terraform.workspace}-checklist-evaluation-processor-queue"
   delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
--- terraform/environment/modules/sqs/ssm.tf
+++ terraform/environment/modules/sqs/ssm.tf
@@ -1,11 +1,23 @@
-resource "aws_ssm_parameter" "criteria_generate_queue_arn" {
-  name  = "/terraform/${terraform.workspace}/sqs/criteria_generate_queue_arn"
+resource "aws_ssm_parameter" "checklist_generation_processor_queue_arn" {
+  name  = "/terraform/${terraform.workspace}/sqs/checklist_generation_processor_queue_arn"
   type  = "String"
-  value = aws_sqs_queue.criteria_generate_queue.arn
+  value = aws_sqs_queue.checklist_generation_processor_queue.arn
 }
 
-resource "aws_ssm_parameter" "criteria_evaluate_queue_arn" {
-  name  = "/terraform/${terraform.workspace}/sqs/criteria_evaluate_queue_arn"
+resource "aws_ssm_parameter" "checklist_generation_processor_queue_url" {
+  name  = "/terraform/${terraform.workspace}/sqs/checklist_generation_processor_queue_url"
   type  = "String"
-  value = aws_sqs_queue.criteria_evaluate_queue.arn
+  value = aws_sqs_queue.checklist_generation_processor_queue.url
+}
+
+resource "aws_ssm_parameter" "checklist_evaluation_processor_queue_arn" {
+  name  = "/terraform/${terraform.workspace}/sqs/checklist_evaluation_processor_queue_arn"
+  type  = "String"
+  value = aws_sqs_queue.checklist_evaluation_processor_queue.arn
+}
+
+resource "aws_ssm_parameter" "checklist_evaluation_processor_queue_url" {
+  name  = "/terraform/${terraform.workspace}/sqs/checklist_evaluation_processor_queue_url"
+  type  = "String"
+  value = aws_sqs_queue.checklist_evaluation_processor_queue.url
 }
