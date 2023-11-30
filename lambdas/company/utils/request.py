9ea20f3c3a66f1532b572755bbe230ef3edb6672
kokiebisu <kokiebisu@icloud.com>
2023-11-29 20:33:58 -0700

    configure cors for company lambda endpoints

--- lambdas/company/utils/request.py
+++ lambdas/company/utils/request.py
@@ -1,3 +1,6 @@
+import json
+
+
 def parse_request_parameter(event, parameter_name):
     """
     Parses a request parameter from an event and returns it as a string.
@@ -13,4 +16,20 @@ def parse_request_parameter(event, parameter_name):
         return parameter
     except Exception as e:
         raise ValueError(f"Error parsing parameter {parameter_name}: {e}")
-    
+
+
+def parse_header(event):
+    """
+    Parses the request header from an event and extracts the origin and host to resolve cors issue
+
+    :param event: The event object containing the request data.
+    :return: origin and the host
+    """
+    try:
+        headers = event['headers']
+        origin = headers.get('origin')
+        if not origin:
+            raise ValueError('Origin not included in headers')
+        return origin
+    except json.JSONDecodeError as e:
+        raise ValueError(f"Invalid JSON in request body: {e}")
