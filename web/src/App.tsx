ffd741197316bdaf06f4f0c3796567833744a417
kokiebisu <kokiebisu@icloud.com>
2023-12-01 22:29:00 -0700

    minor fix

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header/Header';
@@ -28,15 +28,19 @@ function App() {
 
 // TODO: Remove this once confirmed cookies work
 export const Hello = () => {
+  const [data, setData] = useState(null)
   useEffect(() => {
     const id = 'ac7d9ea6-e3e5-4f58-ba26-22e4e2afa9c4'
     const get_position = async () => {
       const response = await axiosInstance.get(`/positions/${id}`)
+      setData(response.data.payload)
       console.log("RESPONSE: ", response)
     }
     get_position()
-  })
-  return <div>hello</div>
+  }, [])
+  return <div>{
+    data ? JSON.stringify(data):null
+    }</div>
 }
 
 export default App;
