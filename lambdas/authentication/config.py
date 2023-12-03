693873943a166ada0cc9aef00401fd607a3133ac
kokiebisu <kokiebisu@icloud.com>
2023-12-02 17:48:13 -0700

    add validate method in Config class

--- lambdas/authentication/config.py
+++ lambdas/authentication/config.py
@@ -2,11 +2,32 @@ import os
 
 
 class Config:
-    ENVIRONMENT = os.environ.get('ENVIRONMENT', 'dev')
-    POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
-    POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
-    POSTGRES_DB = os.environ.get('POSTGRES_DB')
-    POSTGRES_USER = os.environ.get('POSTGRES_USER')
-    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+    """
+    This class handles the configuration settings for the application.
+    """
+    ENVIRONMENT = os.getenv('ENVIRONMENT', 'dev')
+    POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+    POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+    POSTGRES_DB = os.getenv('POSTGRES_DB')
+    POSTGRES_USER = os.getenv('POSTGRES_USER')
+    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+
+    @classmethod
+    def validate(cls):
+        """
+        Validates that all necessary configuration variables are set.
+        Raises an exception if any required configuration is missing.
+        """
+        required_variables = [
+            'POSTGRES_HOST', 'POSTGRES_PORT', 'POSTGRES_DB',
+            'POSTGRES_USER', 'POSTGRES_PASSWORD', 'JWT_SECRET_KEY'
+        ]
+        missing_variables = [variable for variable in required_variables if not getattr(cls, variable)]
+
+        if missing_variables:
+            raise ValueError(f"Missing required configuration variables: {', '.join(missing_variables)}")
+
+
+Config.validate()
--- lambdas/company/config.py
+++ lambdas/company/config.py
@@ -2,10 +2,31 @@ import os
 
 
 class Config:
-    ENVIRONMENT = os.environ.get('ENVIRONMENT')
+    """
+    This class handles the configuration settings for the application.
+    """
+    ENVIRONMENT = os.getenv('ENVIRONMENT', 'dev')
 
-    POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
-    POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
-    POSTGRES_DB = os.environ.get('POSTGRES_DB')
-    POSTGRES_USER = os.environ.get('POSTGRES_USER')
-    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+    POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+    POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+    POSTGRES_DB = os.getenv('POSTGRES_DB')
+    POSTGRES_USER = os.getenv('POSTGRES_USER')
+    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+
+    @classmethod
+    def validate(cls):
+        """
+        Validates that all necessary configuration variables are set.
+        Raises an exception if any required configuration is missing.
+        """
+        required_variables = [
+            'POSTGRES_HOST', 'POSTGRES_PORT', 'POSTGRES_DB',
+            'POSTGRES_USER', 'POSTGRES_PASSWORD'
+        ]
+        missing_variables = [variable for variable in required_variables if not getattr(cls, variable)]
+
+        if missing_variables:
+            raise ValueError(f"Missing required configuration variables: {', '.join(missing_variables)}")
+
+
+Config.validate()
