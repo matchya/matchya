f8f38e5faa26207be04fbff13ec3619b91d14620
kokoaj <kokiebisu@icloud.com>
2023-12-02 18:03:02 -0700

    refactor: set up logic to fail early when config is not loaded properly (#203)

--- lambdas/authorizer/config.py
+++ lambdas/authorizer/config.py
@@ -2,4 +2,23 @@ import os
 
 
 class Config:
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    """
+    This class handles the configuration settings for the application.
+    """
+    ENVIRONMENT = os.getenv('ENVIRONMENT', 'dev')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+
+    @staticmethod
+    def validate():
+        """
+        This method checks if all necessary configuration variables are set.
+        It raises an exception if any required configuration is missing.
+        """
+        required_variables = ['JWT_SECRET_KEY']
+        missing_variables = [variable for variable in required_variables if not getattr(Configuration, variable)]
+
+        if missing_variables:
+            raise ValueError(f"Missing required configuration variables: {', '.join(missing_variables)}")
+
+
+Config.validate()
--- lambdas/checklist/config.py
+++ lambdas/checklist/config.py
@@ -2,21 +2,43 @@ import os
 
 
 class Config:
-    ENVIRONMENT = os.environ.get('ENVIRONMENT')
+    """
+    This class handles the configuration settings for the application.
+    """
+    ENVIRONMENT = os.getenv('ENVIRONMENT', 'dev')
 
     DYNAMO_CRITERIA_TABLE_NAME = f'{ENVIRONMENT}-Criterion'
 
-    POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
-    POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
-    POSTGRES_DB = os.environ.get('POSTGRES_DB')
-    POSTGRES_USER = os.environ.get('POSTGRES_USER')
-    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-    
-    EVALUATION_PROCESSOR_QUEUE_URL = os.environ.get('EVALUATION_PROCESSOR_QUEUE_URL')
-    GENERATION_PROCESSOR_QUEUE_URL = os.environ.get('GENERATION_PROCESSOR_QUEUE_URL')
+    POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+    POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+    POSTGRES_DB = os.getenv('POSTGRES_DB')
+    POSTGRES_USER = os.getenv('POSTGRES_USER')
+    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+
+    EVALUATION_PROCESSOR_QUEUE_URL = os.getenv('EVALUATION_PROCESSOR_QUEUE_URL')
+    GENERATION_PROCESSOR_QUEUE_URL = os.getenv('GENERATION_PROCESSOR_QUEUE_URL')
 
     GITHUB_REST_API_HEADERS = {'Authorization-Type': "Bearer " + os.environ['GITHUB_TOKEN']}
     GITHUB_API_REPO_URL = "https://api.github.com/repos/"
 
     GITHUB_GRAPHQL_API_URL = "https://api.github.com/graphql"
     GITHUB_GRAPHQL_API_HEADERS = {"Authorization": "Bearer " + os.environ['GITHUB_TOKEN']}
+
+    @classmethod
+    def validate(cls):
+        """
+        Validates that all necessary configuration variables are set.
+        Raises an exception if any required configuration is missing.
+        """
+        required_variables = [
+            'POSTGRES_HOST', 'POSTGRES_PORT', 'POSTGRES_DB', 'POSTGRES_USER',
+            'POSTGRES_PASSWORD', 'EVALUATION_PROCESSOR_QUEUE_URL', 'GENERATION_PROCESSOR_QUEUE_URL',
+            'GITHUB_REST_API_HEADERS', 'GITHUB_GRAPHQL_API_HEADERS'
+        ]
+        missing_variables = [variable for variable in required_variables if not getattr(cls, variable)]
+
+        if missing_variables:
+            raise ValueError(f"Missing required configuration variables: {', '.join(missing_variables)}")
+
+
+Config.validate()
