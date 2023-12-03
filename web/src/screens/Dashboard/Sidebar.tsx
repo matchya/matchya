554dbcf5f47c39ba0d2edb8fe30ba8696a7661c1
takeshi8989 <htakeshi0614@gmail.com>
2023-12-02 22:51:08 -0800

    call get company (me) on dashbaord and display the result

--- web/src/screens/Dashboard/Sidebar.tsx
+++ web/src/screens/Dashboard/Sidebar.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import { useCompanyStore } from '../../store/useCompanyStore'
+import { Position } from '../../types';
 
-const Sidebar = () => {
-    const [positions, ] = React.useState(['Software Engineer', 'Backend Developer'])
-    const [selectedPositionIndex, setSelectedPositionIndex] = React.useState<number>(0)
+interface SidebarProps {
+    positions: Position[];
+    selectedPositionId: string;
+    setSelectedPositionId: (id: string) => void;
+}
+
+const Sidebar = ({ positions, selectedPositionId, setSelectedPositionId }: SidebarProps) => {
 
   return (
     <div className='w-full'>
         <div className='mt-10 mx-auto'>
             <div className='flex flex-col mr-4 pl-0'>
-                {positions.map((position, index) => (
-                    <div key={index} className={`rounded-r my-2 cursor-pointer ${index == selectedPositionIndex && 'bg-green-100 border border-l-8 border-l-green-300'}`}
-                    onClick={() => setSelectedPositionIndex(index)}
+                {positions.map((position) => (
+                    <div key={position.id} className={`rounded-r my-2 cursor-pointer ${position.id == selectedPositionId && 'bg-green-100 border border-l-8 border-l-green-300'}`}
+                    onClick={() => setSelectedPositionId(position.id)}
                     >
                         <div className='px-4 py-4 sm:px-6'>
-                            <h3 className={`text-lg leading-6 font-medium ${index == selectedPositionIndex ? 'text-gray-900' : 'text-gray-600'}`}>{position}</h3>
+                            <h3 className={`text-lg leading-6 font-medium ${position.id == selectedPositionId ? 'text-gray-900' : 'text-gray-600'}`}>{position.name}</h3>
                         </div>
                     </div>
                 ))}
