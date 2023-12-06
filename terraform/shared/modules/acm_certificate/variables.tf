8f07a11a2125515dcb56b5fc636f02165a3a7397
kokiebisu <kokiebisu@icloud.com>
2023-12-05 20:07:54 -0700

    remove acm_certificate module

--- terraform/shared/modules/acm_certificate/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "domain_name" {
-  type = string
-}
-
-variable "a_record" {
-  type = any
-}
--- terraform/shared/modules/route53/main.tf
+++ terraform/shared/modules/route53/main.tf
@@ -1,44 +1,31 @@
-# resource "aws_acm_certificate" "example" {
-#   domain_name               = var.domain_name
-#   validation_method         = "DNS"
-# }
-
 resource "aws_route53_zone" "main" {
   name         = var.domain_name
 }
 
-# resource "aws_route53_record" "example" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
-#       name    = dvo.resource_record_name
-#       record  = dvo.resource_record_value
-#       type    = dvo.resource_record_type
-#       zone_id = data.aws_route53_zone.example_com.zone_id
-#     }
-#   }
-
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 60
-#   type            = each.value.type
-#   zone_id         = each.value.zone_id
-# }
-
-# resource "aws_acm_certificate_validation" "example" {
-#   certificate_arn         = aws_acm_certificate.example.arn
-#   validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
-# }
+resource "aws_route53_record" "ns" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "${var.domain_name}"
+  type    = "NS"
+  ttl     = "172800"
+  records = [
+    "ns-1224.awsdns-25.org",
+    "ns-1564.awsdns-03.co.uk",
+    "ns-433.awsdns-54.com",
+    "ns-970.awsdns-57.net"
+  ]
+  allow_overwrite = true
+}
 
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "www.${var.domain_name}"
   type    = "A"
   alias {
-    name                   = var.www_root.website_domain
+    name                   = var.www_root.regional_domain_name
     zone_id                = var.www_root.hosted_zone_id
-    evaluate_target_health = true
+    evaluate_target_health = false
   }
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "root" {
@@ -46,9 +33,10 @@ resource "aws_route53_record" "root" {
   name    = "${var.domain_name}"
   type    = "A"
   alias {
-    name                   = var.root.website_domain
+    name                   = var.root.regional_domain_name
     zone_id                = var.root.hosted_zone_id
-    evaluate_target_health = true
+    evaluate_target_health = false
   }
+  allow_overwrite = true
 }
 
