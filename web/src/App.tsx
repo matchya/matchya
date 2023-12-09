3eca12bb9f1a0fb8e9ed60064cdf446c08edc2ce
kokoaj <kokiebisu@icloud.com>
2023-12-09 16:20:27 -0700

    chore (#268)
    * upgrade to storybook@7.6.4
    
    * add prettier formatter configuration to workspace settings
    
    * remove /hello route

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header/Header';
-import { axiosInstance } from './helper';
-import Dashboard from './screens/Dashboard/Dashboard';
-import Login from './screens/Login';
-import Settings from './screens/Settings/Settings';
+
+import Dashboard from '@/screens/Dashboard/Dashboard';
+import Login from '@/screens/Login';
+import Settings from '@/screens/Settings/Settings';
 
 function App() {
   return (
@@ -14,26 +13,10 @@ function App() {
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="/hello" element={<Hello />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
 }
-
-// TODO: Remove this once confirmed cookies work
-export const Hello = () => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    const id = 'ac7d9ea6-e3e5-4f58-ba26-22e4e2afa9c4';
-    const get_position = async () => {
-      const response = await axiosInstance.get(`/positions/${id}`);
-      setData(response.data.payload);
-      console.log('RESPONSE: ', response);
-    };
-    get_position();
-  }, []);
-  return <div>{data ? JSON.stringify(data) : null}</div>;
-};
 
 export default App;
