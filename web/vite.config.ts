a7dd531c20806f1e629cb677ec3086a3c2bf7447
kokoaj <kokiebisu@icloud.com>
2023-12-09 15:28:27 -0700

    feat(ui): set up shadcn UI framework (#266)
    * run shadcn cli init command
    
    * config tailwind configuration to ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,8 @@
+import path from 'path';
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +10,10 @@ export default defineConfig({
   server: {
     host: '127.0.0.1',
     port: 5173,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
