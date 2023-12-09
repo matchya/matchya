a87cf5bcfa50749d6b28675ab531cd393bd0b768
takeshi8989 <htakeshi0614@gmail.com>
2023-12-09 13:59:15 -0800

    created compression functions for specific files

--- lambdas/checklist/utils/compression.py
+++ lambdas/checklist/utils/compression.py
@@ -1,26 +1,286 @@
+import re
 
 
-def compress_file_content(file_name, file_content):
+# Remove one line comments from file content
+def remove_oneline_comments(extension, file_content):
+    comment_symbols = {
+        "py": "#",
+        "js": "//",
+        "ts": "//",
+        "tsx": "//",
+        "html": "<!--",
+        "css": "/*",
+        "json": "//",
+        "yml": "#",
+        "yaml": "#",
+        "md": "<!--",
+        "gitignore": "#",
+        "csv": "#",
+        "xml": "<!--",
+        "proto": "//",
+        "sql": "--",
+        "graphql": "#",
+        "java": "//",
+        "go": "//",
+        "c": "//",
+        "cpp": "//",
+        "rb": "#",
+        "swift": "//",
+        "kt": "//",
+        "sh": "#",
+        "bat": "REM",
+        "Dockerfile": "#",
+        "tf": "#",
+        "docker-compose.yml": "#",
+        "toml": "#",
+        "tex": "%",
+        "Rmd": "#",
+        "ipynb": "#",
+        "rs": "//",
+        "cs": "//",
+        "php": "//",
+        "mod": "!",
+        "bash_profile": "#",
+        "django": "#",
+        "Makefile": "#",
+        "webpack.config.js": "//",
+        "eslintrc": "//"
+    }
+
+    lines = file_content.split("\n")
+    if extension in comment_symbols:
+        lines = [line for line in file_content if not line.lstrip().startswith(comment_symbols[extension])]
+    file_content = "\n".join(lines)
+    return file_content
+
+
+def compress_default(file_content):
+    file_content = re.sub('\W+', '', file_content)
+    return file_content
+
+
+# Compress file content based on file extension
+def compress_python_file(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line for line in lines
+                        if line.lstrip().startswith("def") or line.lstrip().startswith("class")
+                        or line.lstrip().startswith("import") or line.lstrip().startswith("from")
+                        or line.lstrip().startswith("return")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_js_file(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line for line in lines
+                        if line.lstrip().startswith("function") or line.lstrip().startswith("class")
+                        or line.lstrip().startswith("import") or line.lstrip().startswith("export")
+                        or line.lstrip().startswith("return")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_java_file(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line for line in lines
+                        if line.lstrip().startswith("public") or line.lstrip().startswith("private")
+                        or line.lstrip().startswith("protected") or line.lstrip().startswith("class")
+                        or line.lstrip().startswith("interface") or line.lstrip().startswith("import")
+                        or line.lstrip().startswith("package") or line.lstrip().startswith("return")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_c_file(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line for line in lines
+                        if line.lstrip().startswith("struct") or line.lstrip().startswith("typedef")
+                        or line.startswith("int") or line.startswith("char")
+                        or line.startswith("float") or line.startswith("double")
+                        or line.startswith("void") or line.lstrip().startswith("return")
+                        or line.lstrip().startswith("#include") or line.lstrip().startswith("#define")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_go_file(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line for line in lines
+                        if line.lstrip().startswith("func") or line.lstrip().startswith("type")
+                        or line.lstrip().startswith("import") or line.lstrip().startswith("package")
+                        or line.lstrip().startswith("return")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_csharp_file(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line for line in lines
+                        if line.lstrip().startswith("public") or line.lstrip().startswith("private")
+                        or line.lstrip().startswith("protected") or line.lstrip().startswith("class")
+                        or line.lstrip().startswith("interface") or line.lstrip().startswith("using")
+                        or line.lstrip().startswith("return")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_ruby_file(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line for line in lines
+                        if line.startswith("def") or line.lstrip().startswith("class")
+                        or line.lstrip().startswith("require") or line.lstrip().startswith("return")
+                        or line.startswith("end") or line.lstrip().startswith("module")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_swift_file(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line for line in lines
+                        if line.lstrip().startswith("func") or line.lstrip().startswith("class")
+                        or line.lstrip().startswith("import") or line.lstrip().startswith("return")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_kotlin_file(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line for line in lines
+                        if line.lstrip().startswith("fun") or line.lstrip().startswith("class")
+                        or line.lstrip().startswith("import") or line.lstrip().startswith("return")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_php_file(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line for line in lines
+                        if line.lstrip().startswith("function") or line.lstrip().startswith("class")
+                        or line.lstrip().startswith("require") or line.lstrip().startswith("return")
+                        or line.startswith("end") or line.lstrip().startswith("namespace")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_rust_file(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line for line in lines
+                        if line.lstrip().startswith("fn") or line.lstrip().startswith("struct")
+                        or line.lstrip().startswith("use") or line.lstrip().startswith("return")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+# Compress file content based on file name
+def compress_read_me(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line for line in lines
+                        if not line.contains("![") and not line.contains("<img")
+                        and not line.contains("<video") and not line.contains("<audio")
+                        and not line.contains("<iframe") and not line.contains("<object")
+                        and not line.contains("<embed") and not line.contains("<svg")
+                        and not line.contains("http://") and not line.contains("https://")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_requirements_txt(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line.split("==")[0] for line in lines]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_package_json(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line.split(":")[0] for line in lines]
+    file_content = "\n".join(compressed_lines)
+    file_content = file_content.replace("{", "").replace("}", "").replace(",", "").repalce("\"", "").replace("\'", "")
+    return file_content
+
+
+def compress_gemfile(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line for line in lines if line.lstrip().startswith("gem")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_pom_xml(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line for line in lines if line.lstrip().startswith("<artifactId>")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_go_mod(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line.lstrip().split(" ")[0] for line in lines]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+def compress_packages_config(file_content):
+    lines = file_content.split("\n")
+    compressed_lines = [line.lstrip().split(" ")[1] for line in lines if line.lstrip().startswith("<package")]
+    file_content = "\n".join(compressed_lines)
+    return file_content
+
+
+common_files_compression_func_map = {
+    "README.md": compress_read_me,
+    "requirements.txt": compress_requirements_txt,
+    "package.json": compress_package_json,
+    "package-lock.json": compress_package_json,
+    "composer.json": compress_package_json,
+    "pom.xml": compress_pom_xml,
+    "Gemfile": compress_gemfile,
+    "go.mod": compress_go_mod,
+    "packages.config": compress_packages_config
+}
+
+common_extensions_compression_func_map = {
+    "py": compress_python_file,
+    "js": compress_js_file,
+    "ts": compress_js_file,
+    "tsx": compress_js_file,
+    "java": compress_java_file,
+    "c": compress_c_file,
+    "cpp": compress_c_file,
+    "go": compress_go_file,
+    "cs": compress_csharp_file,
+    "rb": compress_ruby_file,
+    "swift": compress_swift_file,
+    "kt": compress_kotlin_file,
+    "php": compress_php_file,
+    "rs": compress_rust_file
+}
+
+
+# Compress file content
+def compress_file_content(file_name: str, file_content: str):
     """Compress file content using gzip.
 
     param: file_content (str): File content to compress.
     return:str: Compressed file content.
     """
-    if file_name.endswith(".py"):
-        return compress_python_file(file_content)
+    try:
+        extension = file_name.split(".")[-1]
+        file_content = remove_oneline_comments(extension, file_content)
 
-    return file_content.replace(" ", "").replace("\n", "").replace("\"", "").replace("\'", "")
+        if file_name in common_files_compression_func_map:
+            file_content = common_files_compression_func_map[file_name](file_content)
 
+        elif extension in common_extensions_compression_func_map:
+            file_content = common_extensions_compression_func_map[extension](file_content)
 
-def compress_python_file(file_content):
-    """Compress python file content using gzip.
-
-    param: file_content (str): Python file content to compress.
-    return:str: Compressed python file content.
-    """
-
-    # TODO: Add more compression rules for python files.
-    return file_content.replace(" ", "").replace("\n", "").replace("\"", "").replace("\'", "")
-
-
-# TODO: Add more compression rules for other file types.
+        else:
+            print(f"Using default compression {file_content}")
+            file_content = compress_default(file_content)
+            
+        # Limit file content to 4000 characters (~1000 tokens)
+        if len(file_content) > 4000:
+            file_content = file_content[:4000]
+        return file_content
+    except Exception:
+        return file_content
