42e5e9223ef7f56b43cb143af8c95aab165bd88e
takeshi8989 <htakeshi0614@gmail.com>
2023-12-10 23:44:25 -0800

    Add GitHub repository file tree and file reading functionality
    Commit message suggestion:
    Added methods to retrieve the file tree and read the contents of a file from a GitHub repository.

--- lambdas/checklist/client/github.py
+++ lambdas/checklist/client/github.py
@@ -29,6 +29,141 @@ class GithubClient:
             raise RuntimeError("Error getting programming languages used in repository. No languages found.")
         return data
 
+    def get_repository_tree(self, repository_name):
+        """
+        Retrieves the file tree of a GitHub repository.
+
+        :param repository_name: Name of the GitHub repository.
+        :return: A list of files and folders in the repository.
+        """
+        try:
+            branch = self._get_default_branch(repository_name)
+        except Exception:
+            return []
+
+        url = Config.GITHUB_API_REPO_URL + self.github_username + "/" + repository_name + "/git/trees/" + branch + "?recursive=1"
+        try:
+            res = requests.get(url, headers=Config.GITHUB_REST_API_HEADERS)
+        except Exception:
+            raise RuntimeError("Error getting file tree. Request to GitHub API failed.")
+
+        data = json.loads(res.content)
+        if data is None or data.get('tree') is None:
+            raise RuntimeError("Error getting file tree. No tree found.")
+
+        tree = data['tree']
+        return tree
+
+    def read_file(self, repository_name, file_path):
+        """
+        Retrieves the contents of a file from a GitHub repository.
+        
+        :param repository_name: Repository name.
+        :param file_path: Path of the file in the repository.
+        :return: The content of the file as a string.
+        """
+        url = Config.GITHUB_API_REPO_URL + self.github_username + "/" + repository_name + "/contents/" + file_path
+        try:
+            res = requests.get(url, headers=Config.GITHUB_REST_API_HEADERS)
+        except Exception:
+            raise RuntimeError("Error getting file contents. Request to GitHub API failed.")
+
+        data = json.loads(res.content)
+        if data is None or data.get('content') is None:
+            return "(No content found)"
+
+        content_encoded = data['content']
+        decoded = base64.b64decode(content_encoded)
+        content = decoded.decode("utf-8")
+        return content
+
+    @staticmethod
+    def get_package_file_paths_to_read(file_paths, languages):
+        """
+        Filters out the file paths that are not important for the given programming languages.
+        Return up to 10 file paths.
+
+        :param file_paths: A list of file paths.
+        :param languages: A list of programming languages (and their usage details) used in the repository.
+        :return: A list of file paths that are important for the given programming languages.
+        """
+        file_names_for_languages = GithubClient.get_important_file_names_by_languages(languages)
+        package_file_paths = [path for path in file_paths if path.split("/")[-1] in file_names_for_languages]
+
+        max_depth = 5
+        for paths in package_file_paths:
+            if paths.count("/") > max_depth:
+                max_depth = paths.count("/")
+
+        while len(package_file_paths) > 10:
+            max_depth -= 1
+            package_file_paths = [path for path in package_file_paths if path.count("/") == max_depth]
+
+        return package_file_paths
+
+    @staticmethod
+    def get_important_file_names_by_languages(languages):
+        important_file_names = ["README.md"]
+
+        package_file_names = {
+            "Python": "requirements.txt",
+            "JavaScript": "package.json",
+            "Ruby": "Gemfile",
+            "Java": "pom.xml",
+            "Go": "go.mod",
+            "php": "composer.json",
+            "C#": "packages.config",
+            "C++": "CMakeLists.txt",
+            "C": "Makefile",
+            "TypeScript": "package.json",
+            "Shell": "package.json",
+            "Kotlin": "build.gradle",
+            "Rust": "Cargo.toml",
+            "Swift": "Package.swift",
+            "Scala": "build.sbt",
+            "Clojure": "project.clj",
+            "Haskell": "stack.yaml",
+            "Lua": "rockspec",
+            "Perl": "Makefile.PL",
+            "Dart": "pubspec.yaml",
+            "Objective-C": "Podfile",
+            "Elixir": "mix.exs",
+            "HCL": "main.tf",
+            "Groovy": "build.gradle",
+            "PowerShell": "psd1",
+            "R": "DESCRIPTION",
+            "Racket": "info.rkt",
+            "Julia": "Project.toml",
+            "Dockerfile": "Dockerfile",
+        }
+
+        for name, bytes in languages.items():
+            if name in package_file_names:
+                important_file_names.append(package_file_names[name])
+
+        return important_file_names
+
+    def _get_default_branch(self, repo_name):
+        """
+        Fetches the default branch name of a given GitHub repository.
+
+        :param github_username: GitHub username.
+        :param repo_name: Name of the repository.
+        :return: The name of the default branch for the repository.
+        """
+        url = Config.GITHUB_API_REPO_URL + self.github_username + "/" + repo_name
+        try:
+            res = requests.get(url, headers=Config.GITHUB_REST_API_HEADERS)
+        except Exception:
+            raise RuntimeError("Error getting default branch name. Request to GitHub API failed.")
+
+        data = json.loads(res.content)
+        if data is None or data.get('default_branch') is None:
+            raise RuntimeError("Error getting default branch name.")
+        return data.get('default_branch')
+
+    ##############################################################
+
     def get_important_file_paths(self, repo_name, important_file_names):
         """
         Fetches URLs of important files from a GitHub repository.
@@ -223,24 +358,37 @@ class GithubClient:
             raise RuntimeError("Error getting GitHub user. Request to GitHub API failed.")
         return res.status_code == 200
 
-    def _get_default_branch(self, repo_name):
+    @staticmethod
+    def get_organized_folder_structure(paths):
         """
-        Fetches the default branch name of a given GitHub repository.
+        Generates a string containing the folder structure of the repository.
 
-        :param github_username: GitHub username.
-        :param repo_name: Name of the repository.
-        :return: The name of the default branch for the repository.
+        :param repo_tree: A list of files and folders in the repository.
+        :return: A string containing the folder structure of the repository.
         """
-        url = Config.GITHUB_API_REPO_URL + self.github_username + "/" + repo_name
-        try:
-            res = requests.get(url, headers=Config.GITHUB_REST_API_HEADERS)
-        except Exception:
-            raise RuntimeError("Error getting default branch name. Request to GitHub API failed.")
+        tree = {}
 
-        data = json.loads(res.content)
-        if data is None or data.get('default_branch') is None:
-            raise RuntimeError("Error getting default branch name.")
-        return data.get('default_branch')
+        def add_to_tree(base, chain):
+            if len(chain) == 1:
+                base[chain[0]] = base.get(chain[0], {})
+            else:
+                node = base.get(chain[0], {})
+                base[chain[0]] = node
+                add_to_tree(node, chain[1:])
+
+        for path in paths:
+            parts = path.split('/')
+            add_to_tree(tree, parts)
+
+        def tree_to_string(base, level=0):
+            result = ""
+            for k, v in sorted(base.items()):
+                result += "   " * level + k + "\n"
+                if isinstance(v, dict):
+                    result += tree_to_string(v, level + 1)
+            return result
+
+        return tree_to_string(tree).rstrip()
 
     def _is_important_file(self, path, important_file_names, depth=2):
         """
