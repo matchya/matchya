11961b8d6747a851b91c676adf77730e28412647
kokiebisu <kokiebisu@icloud.com>
2023-12-10 18:51:35 -0700

    set up home page component

---
+++ web/src/screens/Home/Home.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+import { useCompanyStore } from '@/store/useCompanyStore';
+
+function Home() {
+  const { id, me } = useCompanyStore();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log('ID: ', id);
+    if (id) return;
+    if (location.pathname === '/auth' || location.pathname === '/') return;
+    getAuthStatus();
+  }, [location.pathname]);
+
+  const getAuthStatus = async () => {
+    try {
+      await me();
+    } catch (error) {
+      console.log('ERROR: ', error);
+      navigate('/auth');
+    }
+  };
+
+  return <div>Home</div>;
+}
+
+export default Home;
