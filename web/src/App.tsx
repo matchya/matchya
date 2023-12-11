4fb0bf1ca33f55eebecfbfd041e19c5c0d48614e
kokiebisu <kokiebisu@icloud.com>
2023-12-10 18:52:06 -0700

    refactor screens import

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,20 +2,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { AuthenticatedLayout, UnAuthenticatedLayout } from './layout';
 
-import Login from '@/screens/Authentication/Authentication';
-import Dashboard from '@/screens/Dashboard/Dashboard';
-import Settings from '@/screens/Settings/Settings';
+import {
+  AuthenticationPage,
+  DashboardPage,
+  HomePage,
+  SettingsPage,
+} from '@/screens';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route element={<AuthenticatedLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/settings" element={<Settings />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
         </Route>
         <Route element={<UnAuthenticatedLayout />}>
-          <Route path="/auth" element={<Login />} />
+          <Route path="/auth" element={<AuthenticationPage />} />
         </Route>
       </Routes>
     </Router>
---
+++ web/src/screens/index.tsx
@@ -0,0 +1,4 @@
+export { default as AuthenticationPage } from './Authentication/Authentication';
+export { default as DashboardPage } from './Dashboard/Dashboard';
+export { default as HomePage } from './Home/Home';
+export { default as SettingsPage } from './Settings/Settings';
