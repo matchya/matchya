a10fea564a4912f9997a205f097efee4430b6abc
kokiebisu <kokiebisu@icloud.com>
2023-12-10 20:00:10 -0700

    fix imports

--- web/src/components/ui/Popover/Popover.stories.tsx
+++ web/src/components/ui/Popover/Popover.stories.tsx
@@ -1,9 +1,6 @@
-import {
-  CaretSortIcon,
-  PlusCircledIcon,
-  CheckIcon,
-} from '@radix-ui/react-icons';
+import { CaretSortIcon } from '@radix-ui/react-icons';
 import { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import {
@@ -13,21 +10,10 @@ import {
 } from '@/components/ui/Avatar/Avatar';
 import { Button } from '@/components/ui/Button/Button';
 import {
-  Command,
-  CommandList,
-  CommandInput,
-  CommandEmpty,
-  CommandGroup,
-  CommandItem,
-  CommandSeparator,
-} from '@/components/ui/Command/Command';
-import { DialogTrigger } from '@/components/ui/Dialog/Dialog';
-import {
   Popover,
   PopoverTrigger,
   PopoverContent,
 } from '@/components/ui/Popover/Popover';
-import { useState } from 'react';
 import { cn } from '@/lib/utils';
 
 const meta: Meta<typeof Popover> = {
@@ -48,7 +34,7 @@ type Story = StoryObj<typeof Popover>;
 export const Default: Story = {
   render: () => {
     const [open, setOpen] = useState(false);
-    const [selectedPosition, setSelectedPosition] = useState({
+    const [selectedPosition] = useState({
       label: '',
       value: '',
     });
