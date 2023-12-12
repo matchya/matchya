e2e60f5b7a37f7da19e16de2c39c41d50f3bb098
takeshi8989 <htakeshi0614@gmail.com>
2023-12-12 15:09:33 -0800

    Add function to hash GitHub access token using bcrypt

--- lambdas/authentication/utils/token.py
+++ lambdas/authentication/utils/token.py
@@ -1,4 +1,5 @@
 import jwt
+import bcrypt
 
 from config import Config
 
@@ -8,3 +9,15 @@ def generate_access_token(company_id):
         "company_id": company_id
     }
     return jwt.encode(payload, Config.JWT_SECRET_KEY, algorithm="HS256")
+
+
+def hash_github_access_token(access_token: str) -> bytes:
+    """
+    Hashes a password using bcrypt.
+
+    :param password: The plaintext password to be hashed.
+    :return: The hashed password as a byte string.
+    """
+    token_bytes = access_token.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(token_bytes, salt)
