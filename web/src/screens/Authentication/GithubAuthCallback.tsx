06c4a0a72ab3de324af077cf9a2b38bf4b620cc2
takeshi8989 <htakeshi0614@gmail.com>
2023-12-17 13:14:59 -0800

    Add login failure handling and UI improvements to GithubAuthCallback.tsx

--- web/src/screens/Authentication/GithubAuthCallback.tsx
+++ web/src/screens/Authentication/GithubAuthCallback.tsx
@@ -1,10 +1,15 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
+import { Icons } from '../../components/ui/Icons/Icons';
+
+import { Button } from '@/components/ui/Button/Button';
 import { axiosInstance } from '@/helper';
+
 const GithubAuthCallback = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const [loginFailed, setLoginFailed] = useState(false);
 
   useEffect(() => {
     const code = new URLSearchParams(location.search).get('code');
@@ -21,12 +26,35 @@ const GithubAuthCallback = () => {
         navigate('/dashboard');
       }
     } catch (error) {
-      console.error('Github Login Failed: ', error);
-      navigate('/auth');
+      setLoginFailed(true);
+      setTimeout(() => {
+        setLoginFailed(false);
+        navigate('/auth');
+      }, 5000);
     }
   };
 
-  return <div>Logging in with GitHub...</div>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center mb-5">
+        {!loginFailed && (
+          <Icons.spinner className="mb-3 h-10 w-10 animate-spin" />
+        )}
+        <p className="text-xl">
+          {!loginFailed ? 'Logging in with GitHub...' : 'Login with GitHub Failed. Please try again.'}
+        </p>
+        {loginFailed && (
+          <Button
+            variant="outline"
+            className="mt-5 h-12 text-xl bg-black text-white"
+            onClick={() => navigate('/auth')}
+          >
+            Retry Login
+          </Button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default GithubAuthCallback;
