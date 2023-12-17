616d5db131fb0ad3f1172d1938e566eefa5b6fcf
kokoaj <kokiebisu@icloud.com>
2023-12-16 17:59:45 -0700

    feat(ui): set up separator component (#331)
    * set up seperator component
    
    * set up story

---
+++ web/src/components/ui/Seperator/Seperator.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { Separator as Component } from './Seperator';
+
+const meta: Meta<typeof Component> = {
+  title: 'Component',
+  component: Component,
+  decorators: [
+    Story => (
+      <Router>
+        <Story />
+      </Router>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Component>;
+
+export const Seperator: Story = {
+  render: () => (
+    <div>
+      <div className="space-y-1">
+        <h4 className="text-sm font-medium leading-none">Radix Primitives</h4>
+        <p className="text-sm text-muted-foreground">
+          An open-source UI component library.
+        </p>
+      </div>
+      <Component className="my-4" />
+      <div className="flex h-5 items-center space-x-4 text-sm">
+        <div>Blog</div>
+        <Component orientation="vertical" />
+        <div>Docs</div>
+        <Component orientation="vertical" />
+        <div>Source</div>
+      </div>
+    </div>
+  ),
+};
