c8aae37b48f09573dd4ee7bf17f22863055b396d
takeshi8989 <htakeshi0614@gmail.com>
2023-12-17 20:24:08 -0800

    Update Home component with new images and navigation

--- web/src/screens/Home/Home.tsx
+++ web/src/screens/Home/Home.tsx
@@ -1,6 +1,17 @@
-import sampleImage from './sample.png'
+import { useNavigate } from 'react-router-dom';
+
+import sampleCandidates from './sample-candidates.png';
+import sampleChecklist from './sample-checklist.png';
+
+import { Button } from '@/components/ui/Button/Button';
 
 function Home() {
+  const navigate = useNavigate();
+
+  const navigateAuth = () => {
+    navigate('/auth');
+  };
+
   return (
     <div className="w-full flex flex-col items-center">
       <div className="text-center mt-16 w-full flex flex-col justify-center">
@@ -16,25 +27,50 @@ function Home() {
           it.
         </p>
       </div>
-      <div className="flex justify-around items-center mt-32 w-full px-20">
+
+      <div className="w-full flex justify-around items-center mt-16 py-10 px-28 bg-green-100">
         <div className="w-1/2 flex justify-center items-center mx-20">
-          <img src={sampleImage} alt="ai" />
+          <img src={sampleChecklist} alt="checklist-screen" />
         </div>
-        <div className="w-1/2 flex items-center">
+        <div className="w-1/2">
           <p className="text-md indent-6">
             At the heart of Matchya is an innovative algorithm, meticulously
             engineered for two crucial tasks. Initially, it conducts a deep dive
             into your company's GitHub data, extracting key skills indicative of
             your operational requirements. This intelligent analysis results in
             a custom-fit checklist, perfectly aligned with your expectations.
+          </p>
+        </div>
+      </div>
+
+      <div className="w-full flex justify-around items-center py-10 px-28 bg-green-200">
+        <div className="w-1/2 flex justify-center items-center mx-20">
+          <img src={sampleCandidates} alt="candidates-screen" />
+        </div>
+        <div className="w-1/2">
+          <p className="text-md indent-6">
             The algorithm then shifts focus to candidate evaluation, rigorously
             comparing their GitHub footprint with this checklist. It evaluates
             their technical contributions, coding quality, and project
             relevance, providing a nuanced understanding of their suitability.
             This dual-phase approach by Matchya not only streamlines the
             recruitment process but also ensures a highly accurate alignment of
-            candidate capabilities with your company's technical demands.'
+            candidate capabilities with your company's technical demands.
           </p>
+        </div>
+      </div>
+
+      <div className="bg-white w-full flex justify-center my-12">
+        <div>
+          <Button
+            variant="default"
+            size="lg"
+            type="button"
+            className="m-6 rounded-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-4 px-4"
+            onClick={navigateAuth}
+          >
+            Get Started
+          </Button>
         </div>
       </div>
     </div>
