10431cb6d705ed39114f416614aac8bd17e73a48
kokiebisu <kokiebisu@icloud.com>
2023-12-18 19:12:02 -0700

    move logic out of checklist component

--- web/src/components/ui/Sheet/ChecklistSheet/ChecklistSheet.stories.tsx
+++ web/src/components/ui/Sheet/ChecklistSheet/ChecklistSheet.stories.tsx
@@ -5,6 +5,7 @@ import { ChecklistSheet as Component } from './ChecklistSheet';
 
 import { Button } from '@/components/ui/Button/Button';
 import { Sheet, SheetTrigger } from '@/components/ui/Sheet/Sheet';
+import { Position } from '@/types';
 
 const meta: Meta<typeof Component> = {
   title: 'Component/Sheet',
@@ -23,12 +24,70 @@ export default meta;
 type Story = StoryObj<typeof Component>;
 
 export const Checklist: Story = {
-  render: () => (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button variant="outline">Open</Button>
-      </SheetTrigger>
-      <Component />
-    </Sheet>
-  ),
+  render: () => {
+    const position: Position = {
+      id: '1',
+      name: 'Position 1',
+      checklist_status: 'succeeded',
+      checklist: {
+        id: '1',
+        repository_names: ['repo1', 'repo2'],
+        candidates: [
+          {
+            id: '1',
+            first_name: 'John',
+            last_name: 'Doe',
+            email: 'john.doe@example.com',
+            github_username: 'johndoe',
+            total_score: 85,
+            summary: 'Good candidate',
+            assessments: [
+              {
+                criterion: {
+                  id: '1',
+                  message: 'Criteria 1',
+                  keywords: ['keyword1', 'keyword2'],
+                  created_at: '2022-01-01T00:00:00Z',
+                },
+                score: 5,
+                reason: '',
+              },
+              {
+                criterion: {
+                  id: '2',
+                  message: 'Criteria 2',
+                  keywords: ['keyword3', 'keyword4'],
+                  created_at: '2022-01-02T00:00:00Z',
+                },
+                score: 4,
+                reason: '',
+              },
+            ],
+          },
+        ],
+        criteria: [
+          {
+            id: '1',
+            message: 'Criteria 1',
+            keywords: ['keyword1', 'keyword2'],
+            created_at: '2022-01-01T00:00:00Z',
+          },
+          {
+            id: '2',
+            message: 'Criteria 2',
+            keywords: ['keyword3', 'keyword4'],
+            created_at: '2022-01-02T00:00:00Z',
+          },
+        ],
+      },
+    };
+    return (
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline">Open</Button>
+        </SheetTrigger>
+        <Component selectedPosition={position} />
+      </Sheet>
+    );
+  },
 };
