35d4ac41a54bca8eb315ecaaf039787c1ab83e31
kokoaj <kokiebisu@icloud.com>
2023-12-18 20:25:46 -0700

    feat(ui): refine add candidate with shadcn framework (#350)
    * add candidate dialog component
    
    * set up story
    
    * fix button import
    
    * fix button component
    
    * fix main nav component

---
+++ web/src/components/ui/Dialog/AddCandidateDialog/AddCandidateDialog.stories.tsx
@@ -0,0 +1,35 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { Dialog } from '../Dialog';
+
+import { AddCandidateDialog as Component } from './AddCandidateDialog';
+
+const meta: Meta<typeof Component> = {
+  title: 'Component/Dialog',
+  component: Component,
+  decorators: [
+    Story => (
+      <Router>
+        <Story />
+      </Router>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Component>;
+
+export const AddCandidate: Story = {
+  render: () => (
+    <Dialog open={true} onOpenChange={() => {}}>
+      <Component
+        shouldOpen={true}
+        isLoading={false}
+        onClose={() => {}}
+        onSubmit={() => {}}
+      />
+    </Dialog>
+  ),
+};
