ee8a7a6db940503b197bb95ef599537aa303b80b
kokiebisu <kokiebisu@icloud.com>
2023-12-18 18:46:21 -0700

    move all the logic extracted from header/position component to authenticated layout component

--- web/src/layout/AuthenticatedLayout.tsx
+++ web/src/layout/AuthenticatedLayout.tsx
@@ -1,11 +1,64 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 import { Header } from '@/components/ui/Header/Header';
+import { axiosInstance } from '@/helper';
+import { useCompanyStore } from '@/store/useCompanyStore';
+import { usePositionStore } from '@/store/usePositionStore';
 
 const Layout = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { id, resetAll, me, name, email } = useCompanyStore();
+  const { positions, selectedPosition, selectPosition } = usePositionStore();
+
+  useEffect(() => {
+    if (id) return;
+    getAuthStatus();
+  }, [location.pathname]);
+
+  const getAuthStatus = async () => {
+    try {
+      await me();
+    } catch (error) {
+      navigateToAuth();
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await axiosInstance.post('/logout');
+      resetAll();
+      navigate('/');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const navigateToAuth = () => {
+    navigate('/auth');
+  };
+
+  const abbreviateName = () => {
+    const nameParts = name.split(' ');
+    if (nameParts.length === 1) {
+      return nameParts[0].charAt(0);
+    } else {
+      return nameParts[0].charAt(0) + nameParts[1].charAt(0);
+    }
+  };
+
   return (
     <>
-      <Header />
+      <Header
+        abbreviatedName={abbreviateName()}
+        companyName={name}
+        email={email}
+        handleLogout={handleLogout}
+        positions={positions}
+        selectPosition={selectPosition}
+        selectedPosition={selectedPosition}
+      />
       <Outlet />
     </>
   );
