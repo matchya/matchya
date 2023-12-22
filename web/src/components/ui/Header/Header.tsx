31873e0e622531e92b47bed788320ea0cd8b5ff9
takeshi8989 <htakeshi0614@gmail.com>
2023-12-22 12:32:47 -0800

    Refactor Header component to use new authentication flow

--- web/src/components/ui/Header/Header.tsx
+++ web/src/components/ui/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import { Avatar } from '../Avatar/Avatar';
 import { Button } from '../Button/Button';
 import {
@@ -8,45 +10,72 @@ import { UserNavDropdownMenu } from '../DropdownMenu/UserNavDropdownMenu/UserNav
 import { PositionSwitcher } from '../PositionSwitcher/PositionSwitcher';
 
 import { MainNav } from '@/components/ui/MainNav/MainNav';
-import { Position } from '@/types';
+import { axiosInstance } from '@/helper';
+import { useCompanyStore } from '@/store/useCompanyStore';
+import { usePositionStore } from '@/store/usePositionStore';
 
 interface HeaderProps {
-  abbreviatedName: string;
-  companyName: string;
-  email: string;
-  handleLogout: () => void;
-  positions: Position[];
-  selectPosition: (position: Position) => void;
-  selectedPosition: Position;
+  authenticated: boolean;
 }
 
-export const Header = ({
-  abbreviatedName,
-  companyName,
-  email,
-  handleLogout,
-  positions,
-  selectPosition,
-  selectedPosition,
-}: HeaderProps) => {
+export const Header = ({ authenticated }: HeaderProps) => {
+  const navigate = useNavigate();
+  const { resetAll, name, email } = useCompanyStore();
+  const { positions, selectedPosition, selectPosition } = usePositionStore();
+
+  const handleLogout = async () => {
+    try {
+      await axiosInstance.post('/logout');
+      resetAll();
+      navigate('/');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const abbreviateName = () => {
+    const nameParts = name.split(' ');
+    if (nameParts.length === 1) {
+      return nameParts[0].charAt(0);
+    } else {
+      return nameParts[0].charAt(0) + nameParts[1].charAt(0);
+    }
+  };
+
+  if (!authenticated && !name) {
+    return (
+      <div className="border-b">
+        <div className="flex h-16 items-center px-4">
+          <div className="ml-auto mr-6 flex items-center space-x-4">
+            <Button variant="ghost" onClick={() => navigate('/auth')}>
+              Login
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <PositionSwitcher
-          positions={positions}
-          selectedPosition={selectedPosition}
-          selectPosition={selectPosition}
-        />
-        <MainNav className="mx-6" />
+        {selectedPosition && authenticated && (
+          <PositionSwitcher
+            positions={positions}
+            selectedPosition={selectedPosition}
+            selectPosition={selectPosition}
+          />
+        )}
+        {authenticated && <MainNav className="mx-6" />}
         <div className="ml-auto flex items-center space-x-4">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-                <Avatar altName={abbreviatedName} />
+                <Avatar altName={abbreviateName()} />
               </Button>
             </DropdownMenuTrigger>
             <UserNavDropdownMenu
-              companyName={companyName}
+              companyName={name}
               companyEmail={email}
               onLogout={handleLogout}
             />
