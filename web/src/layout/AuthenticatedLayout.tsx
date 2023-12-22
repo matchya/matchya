6e86f5119866d0bd09de4c1d3cf7d10fc9c3481f
takeshi8989 <htakeshi0614@gmail.com>
2023-12-22 12:35:17 -0800

    Refactor layout components and update header usage

--- web/src/layout/AuthenticatedLayout.tsx
+++ web/src/layout/AuthenticatedLayout.tsx
@@ -2,15 +2,12 @@ import { useEffect } from 'react';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 import { Header } from '@/components/ui/Header/Header';
-import { axiosInstance } from '@/helper';
 import { useCompanyStore } from '@/store/useCompanyStore';
-import { usePositionStore } from '@/store/usePositionStore';
 
 const Layout = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const { id, resetAll, me, name, email } = useCompanyStore();
-  const { positions, selectedPosition, selectPosition } = usePositionStore();
+  const { id, me } = useCompanyStore();
 
   useEffect(() => {
     if (id) return;
@@ -25,40 +22,13 @@ const Layout = () => {
     }
   };
 
-  const handleLogout = async () => {
-    try {
-      await axiosInstance.post('/logout');
-      resetAll();
-      navigate('/');
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   const navigateToAuth = () => {
     navigate('/auth');
   };
 
-  const abbreviateName = () => {
-    const nameParts = name.split(' ');
-    if (nameParts.length === 1) {
-      return nameParts[0].charAt(0);
-    } else {
-      return nameParts[0].charAt(0) + nameParts[1].charAt(0);
-    }
-  };
-
   return (
     <>
-      <Header
-        abbreviatedName={abbreviateName()}
-        companyName={name}
-        email={email}
-        handleLogout={handleLogout}
-        positions={positions}
-        selectPosition={selectPosition}
-        selectedPosition={selectedPosition}
-      />
+      <Header authenticated={true} />
       <Outlet />
     </>
   );
