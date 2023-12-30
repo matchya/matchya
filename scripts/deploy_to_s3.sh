ec0d28a8a43701557d61610994b3e79ff741ab70
kokiebisu <kokiebisu@icloud.com>
2023-12-29 17:37:02 -0700

    fix(terraform): update s3 deployment script to find cloudwatch distribution by bucket name

--- scripts/deploy_to_s3.sh
+++ scripts/deploy_to_s3.sh
@@ -39,7 +39,7 @@ if [ -z "$environment" ]; then
 fi
 
 # Get the distribution id from SSM parameter
-distribution_id=$(aws ssm get-parameter --name "/terraform/shared/${environment}/www/cloudfront_distribution_id" --query "Parameter.Value" --output text)
+distribution_id=$(aws ssm get-parameter --name "/terraform/shared/${bucket_name}/www/cloudfront_distribution_id" --query "Parameter.Value" --output text)
 if [ -z "$distribution_id" ]; then
     echo "Distribution ID not found. Exiting without deploying to S3."
     exit 1
