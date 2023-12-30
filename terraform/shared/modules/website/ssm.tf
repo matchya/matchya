3be6879fbb7543e3de3cb94c8b828db33a743f62
kokiebisu <kokiebisu@icloud.com>
2023-12-29 17:40:48 -0700

    feat(terraform): set up client for staging

--- terraform/shared/modules/website/ssm.tf
+++ terraform/shared/modules/website/ssm.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "cloudfront_distribution_id" {
-  name  = "/terraform/shared/production/www/cloudfront_distribution_id"
+  name  = "/terraform/shared/${var.domain_name}/cloudfront_distribution_id"
   type  = "String"
-  value = aws_cloudfront_distribution.www_root.id
+  value = aws_cloudfront_distribution.www.id
 }
