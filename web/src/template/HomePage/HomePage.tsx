a7f4752bc3fff0c9d6ef169249daeada8407367f
takeshi8989 <htakeshi0614@gmail.com>
2024-01-03 21:57:05 -0800

    created theme

--- web/src/template/HomePage/HomePage.tsx
+++ web/src/template/HomePage/HomePage.tsx
@@ -1,3 +1,4 @@
+import hpImage from '@/assets/hp-image.jpg';
 import sampleChecklist from '@/assets/sample-checklist.png';
 import sampleEvaluations from '@/assets/sample-evaluations.png';
 import { Button } from '@/components';
@@ -8,19 +9,48 @@ interface HomePageTemplateProps {
 
 const HomePageTemplate = ({ onNavigateToAuth }: HomePageTemplateProps) => {
   return (
-    <div className="w-full flex flex-col items-center">
-      <div className="text-center mt-16 w-full flex flex-col justify-center">
-        <h1 className="text-4xl font-bold">Find Tech Talent with AI Insight</h1>
-        <p className="text-md mt-4 w-3/5 mx-auto">
-          Matchya is the key to unlocking a new era of talent acquisition for
-          companies seeking skilled software engineers. Leveraging AI, Matchya
-          transforms the hiring landscape, making it faster, more precise, and
-          incredibly efficient. With our platform, companies can discover the
-          perfect match in candidates, ensuring a seamless fit of skills and
-          requirements. Welcome to the future of tech recruitment, where finding
-          the right talent is as smart and swift as the technology that powers
-          it.
-        </p>
+    <div className="w-full h-2/3 flex flex-col items-center">
+      <div className='flex'>
+        <div className=" mt-16 w-1/3 ml-auto mt-32">
+          <h2 className="text-2xl font-bold">AI Recruiting Software</h2>
+
+          <h1 className="text-5xl font-bold mt-2">
+            Find Tech Talent with AI Insight
+          </h1>
+          <p className="text-md mt-6">
+            Matchya is the key to unlocking a new era of talent acquisition for
+            companies seeking skilled software engineers. Leveraging AI, Matchya
+            transforms the hiring landscape, making it faster, more precise, and
+            incredibly efficient. With our platform, companies can discover the
+            perfect match in candidates, ensuring a seamless fit of skills and
+            requirements. Welcome to the future of tech recruitment, where
+            finding the right talent is as smart and swift as the technology
+            that powers it.
+          </p>
+          <div className='buttons mt-10'>
+            <Button
+              variant="default"
+              size="lg"
+              type="button"
+              className="m-6 bg-blue-500 hover:bg-blue-600 text-white font-bold py-4 px-4 h-16 w-48 text-xl"
+              onClick={onNavigateToAuth}
+            >
+              Get Started
+            </Button>
+            <Button
+              variant="outline"
+              size="lg"
+              type="button"
+              className="m-6 border border-2 border-blue-600 text-blue-600 font-bold py-4 px-4 h-16 w-48 text-xl"
+              onClick={() => {}}
+            >
+              Contact Us
+            </Button>
+          </div>
+        </div>
+        <div className="w-1/2 flex justify-center items-center mx-20">
+          <img src={hpImage} alt="checklist-screen" className='rounded-3xl h-3/4' />
+          </div>
       </div>
 
       <div className="w-full flex justify-around items-center mt-16 py-10 px-28 bg-green-100">
