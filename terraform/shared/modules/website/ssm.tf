9a51ea610272eba4c2e8b5b1bcd68c155f48ebd7
kokiebisu <kokiebisu@icloud.com>
2024-01-04 11:06:45 -0700

    fix(terraform): update cloud distribution id ssm param with www. prefix

--- terraform/shared/modules/website/ssm.tf
+++ terraform/shared/modules/website/ssm.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "cloudfront_distribution_id" {
-  name  = "/terraform/shared/${var.domain_name}/cloudfront_distribution_id"
+  name  = "/terraform/shared/www.${var.domain_name}/cloudfront_distribution_id"
   type  = "String"
   value = aws_cloudfront_distribution.www.id
 }
