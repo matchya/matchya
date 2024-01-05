fab961c272ae916e3828b597d7a57ebaa8e43b5d
kokiebisu <kokiebisu@icloud.com>
2024-01-04 23:49:01 -0700

    fix(terraform): move bastion host resource into shared

--- terraform/shared/main.tf
+++ terraform/shared/main.tf
@@ -1,3 +1,10 @@
+module "ec2" {
+  source = "./modules/ec2"
+
+  public_ec2_security_group = module.vpc.public_ec2_security_group
+  public_subnet_1 = module.vpc.public_subnet_1
+}
+
 module "iam" {
   source = "./modules/iam"
 
