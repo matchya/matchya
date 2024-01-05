00288d323c22f2cecde01f0e8f7405a32b41cf66
TAKESHI <109770597+takeshi8989@users.noreply.github.com>
2024-01-04 19:05:35 -0800

    feat(web): set up google login (#463)

--- web/src/screens/AuthenticationPage.tsx
+++ web/src/screens/AuthenticationPage.tsx
@@ -1,23 +1,35 @@
 import { useState } from 'react';
 
-import { clientEndpoint, githubClientId } from '@/config/env';
+import { clientEndpoint, githubClientId, googleClientId } from '@/config/env';
 import { AuthenticationPageTemplate } from '@/template';
 
 const AuthenticationPage = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isGitHubLoading, setIsGitHubLoading] = useState<boolean>(false);
+  const [isGoogleLoading, setIsGoogleLoading] = useState<boolean>(false);
 
   const handleGithubLogin = () => {
-    setIsLoading(true);
+    setIsGitHubLoading(true);
     const redirectUri = `${clientEndpoint}/auth/github/callback`;
     const loginUrl = `https://github.com/login/oauth/authorize?client_id=${githubClientId}&redirect_uri=${redirectUri}&scope=repo,user`;
 
     window.location.href = loginUrl;
   };
 
+  const handleGoogleLogin = () => {
+    setIsGoogleLoading(true);
+    // const redirectUri = `${clientEndpoint}/auth/google/callback`;
+    const redirectUri = 'http://127.0.0.1:5173/auth/google/callback';
+    const loginUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${googleClientId}&redirect_uri=${redirectUri}&response_type=token&scope=openid%20email%20profile`;
+
+    window.location.href = loginUrl;
+  };
+
   return (
     <AuthenticationPageTemplate
-      isLoading={isLoading}
+      isGitHubLoading={isGitHubLoading}
+      isGoogleLoading={isGoogleLoading}
       onGithubLogin={handleGithubLogin}
+      onGoogleLogin={handleGoogleLogin}
     />
   );
 };
