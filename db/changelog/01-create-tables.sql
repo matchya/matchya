f79d907f13d41f409391c46aaeea2c22d04c288d
TAKESHI <109770597+takeshi8989@users.noreply.github.com>
2024-01-05 21:06:21 -0800

    feat(db): organized liquibase changelogs (#480)

--- db/changelog/01-create-tables.sql
+++ db/changelog/01-create-tables.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS company (
 	name varchar(255) NOT NULL,
 	email varchar(255) NOT NULL unique,
 	github_username varchar(255),
+	github_access_token bytea,
 	password bytea,
 	created_at timestamp DEFAULT CURRENT_TIMESTAMP
 );
@@ -16,6 +17,9 @@ CREATE TABLE IF NOT EXISTS position (
 	id varchar(255) not null primary key,
 	company_id varchar(255),
 	name varchar(255),
+	type varchar(30),
+	level varchar(30),
+	checklist_generation_status varchar(30) DEFAULT 'unscheduled',
 	created_at timestamp default current_timestamp,
 	foreign key (company_id) references company(id)
 );
@@ -32,21 +36,32 @@ CREATE TABLE IF NOT EXISTS candidate (
 );
 --rollback DROP TABLE IF EXISTS candidate;
 
-
 --changeset author:4
-CREATE TABLE IF NOT EXISTS candidate_result (
+CREATE TABLE IF NOT EXISTS checklist (
 	id varchar(255) not null primary key,
 	position_id varchar(255),
-	candidate_id varchar(255),
-	total_score int,
-	summary varchar(1023),
+	name varchar(255),
+	status varchar(30) DEFAULT 'scheduled',
 	created_at timestamp default CURRENT_TIMESTAMP,
-	foreign key (position_id) references Position(id),
-	foreign key (candidate_id) references candidate(id)
+	foreign key (position_id) references position(id)
+);
+--rollback DROP TABLE IF EXISTS checklist;
+
+--changeset author:5
+CREATE TABLE IF NOT EXISTS candidate_result (
+	id varchar(255) not null primary key,
+	candidate_id varchar(255),
+	checklist_id varchar(255),
+	total_score float,
+	summary varchar(1023),
+	status varchar(30) DEFAULT 'scheduled',
+	created_at timestamp default CURRENT_TIMESTAMP,
+	foreign key (candidate_id) references candidate(id),
+	foreign key (checklist_id) references checklist(id)
 );
 --rollback DROP TABLE IF EXISTS candidate_result;
 
---changeset author:5
+--changeset author:6
 CREATE TABLE IF NOT EXISTS assessment_criteria (
 	id varchar(255) not null primary key,
 	candidate_result_id varchar(255),
@@ -57,7 +72,7 @@ CREATE TABLE IF NOT EXISTS assessment_criteria (
 );
 --rollback DROP TABLE IF EXISTS assessment_criteria;
 
---changeset author:6
+--changeset author:7
 CREATE TABLE IF NOT EXISTS company_repository (
 	id varchar(255) not null primary key,
 	company_id varchar(255),
@@ -66,42 +81,8 @@ CREATE TABLE IF NOT EXISTS company_repository (
 );
 --rollback DROP TABLE IF EXISTS company_repository;
 
---changeset author:7
-CREATE TABLE IF NOT EXISTS position_repository (
-	id varchar(255) not null primary key,
-	position_id varchar(255),
-	repository_name varchar(255),
-	foreign key (position_id) references position(id)
-);
---rollback DROP TABLE IF EXISTS position_repository;
 
 --changeset author:8
-CREATE TABLE IF NOT EXISTS checklist (
-	id varchar(255) not null primary key,
-	position_id varchar(255),
-	name varchar(255),
-	created_at timestamp default CURRENT_TIMESTAMP,
-	foreign key (position_id) references position(id)
-);
---rollback DROP TABLE IF EXISTS checklist;
-
---changeset author:9
-ALTER TABLE candidate_result DROP COLUMN position_id;
-ALTER TABLE candidate_result ADD COLUMN checklist_id varchar(255);
-ALTER TABLE candidate_result ADD FOREIGN KEY (checklist_id) REFERENCES checklist(id);
---rollback ALTER TABLE candidate_result DROP COLUMN checklist_id;
---rollback ALTER TABLE candidate_result ADD COLUMN position_id varchar(255);
---rollback ALTER TABLE candidate_result DROP FOREIGN KEY checklist_id;
-
---changeset author:12
-DROP TABLE IF EXISTS position_repository;
---rollback CREATE TABLE IF NOT EXISTS position_repository (
---rollback 	id varchar(255) not null primary key,
---rollback 	position_id varchar(255),
---rollback 	repository_name varchar(255),
---rollback 	foreign key (position_id) references position(id)
---rollback );
-
 CREATE TABLE IF NOT EXISTS checklist_repository (
 	id varchar(255) not null primary key,
 	checklist_id varchar(255),
@@ -109,30 +90,3 @@ CREATE TABLE IF NOT EXISTS checklist_repository (
 	foreign key (checklist_id) references checklist(id)
 );
 --rollback DROP TABLE IF EXISTS checklist_repository;
-
---changeset author:13
-ALTER TABLE position ADD COLUMN checklist_generation_status varchar(30) DEFAULT 'unscheduled';
---rollback ALTER TABLE position DROP COLUMN checklist_generation_status;
-
---changeset author:14
--- change the type of total_score from int to float
-ALTER TABLE candidate_result ALTER COLUMN total_score TYPE float;
---rollback ALTER TABLE candidate_result ALTER COLUMN total_score TYPE int;
-
---changeset author:15
-ALTER TABLE company ADD COLUMN github_access_token bytea;
---rollback ALTER TABLE company DROP COLUMN github_access_token;
-
---changeset author:16
-ALTER TABLE candidate_result ADD COLUMN status varchar(30) DEFAULT 'scheduled';
---rollback ALTER TABLE candidate_result DROP COLUMN status;
-
---changeset author:17
-ALTER TABLE checklist ADD COLUMN status varchar(30) DEFAULT 'scheduled';
---rollback ALTER TABLE checklist DROP COLUMN status;
-
---changeset author:18
-ALTER TABLE position ADD COLUMN type varchar(30);
-ALTER TABLE position ADD COLUMN level varchar(30);
---rollback ALTER TABLE position DROP COLUMN type;
---rollback ALTER TABLE position DROP COLUMN level;
