1579fbc721a46fcc425a63969b02b14b6670ebe2
kokiebisu <kokiebisu@icloud.com>
2024-01-06 14:39:37 -0700

    fix(ci): suppress terraform plan changes for api gateway when nothing is changed

--- terraform/environment/modules/apigateway/main.tf
+++ terraform/environment/modules/apigateway/main.tf
@@ -6,6 +6,10 @@ resource "aws_api_gateway_gateway_response" "default_4xx" {
   rest_api_id    = aws_api_gateway_rest_api.default.id
   response_type  = "DEFAULT_4XX"
 
+  response_templates = {
+    "application/json" = "{\"message\":$context.error.messageString}"
+  }
+
   response_parameters = {
     "gatewayresponse.header.Access-Control-Allow-Origin" = "'${var.client_origin}'"
     "gatewayresponse.header.Access-Control-Allow-Headers" = "'*'"
@@ -18,6 +22,10 @@ resource "aws_api_gateway_gateway_response" "default_5xx" {
   rest_api_id    = aws_api_gateway_rest_api.default.id
   response_type  = "DEFAULT_5XX"
 
+  response_templates = {
+    "application/json" = "{\"message\":$context.error.messageString}"
+  }
+
   response_parameters = {
     "gatewayresponse.header.Access-Control-Allow-Origin" = "'${var.client_origin}'"
     "gatewayresponse.header.Access-Control-Allow-Headers" = "'*'"
@@ -29,6 +37,11 @@ resource "aws_api_gateway_gateway_response" "default_5xx" {
 resource "aws_api_gateway_gateway_response" "unauthorized_response" {
   rest_api_id    = aws_api_gateway_rest_api.default.id
   response_type  = "UNAUTHORIZED"
+
+  response_templates = {
+    "application/json" = "{\"message\":$context.error.messageString}"
+  }
+  status_code = "401"
 
   response_parameters = {
     "gatewayresponse.header.Access-Control-Allow-Origin" = "'${var.client_origin}'"
