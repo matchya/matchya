e6b6a7d7f72e589a7a20f73974d21156daf87571
kokiebisu <kokiebisu@icloud.com>
2024-01-09 20:16:23 -0700

    rename bucket to {environment}-data-candidate-response

--- terraform/environment/modules/s3/main.tf
+++ terraform/environment/modules/s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "candidate_response" {
-  bucket = "${terraform.workspace}-candidate-response"
+  bucket = "${terraform.workspace}-data-candidate-response"
 
   lifecycle {
     create_before_destroy = true
