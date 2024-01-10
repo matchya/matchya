0aa2a9a4d94bf1400c0882f8a318d3bf1148c963
kokiebisu <kokiebisu@icloud.com>
2024-01-09 21:14:14 -0700

    integrate route53 module

--- terraform/shared/variables.tf
+++ terraform/shared/variables.tf
@@ -2,3 +2,8 @@ variable "domain_name" {
   description = "Map of domain names for different environments"
   type = map(string)
 }
+
+variable "vercel_records" {
+  description = "Map of vercel records for production"
+  type = map(string)
+}
