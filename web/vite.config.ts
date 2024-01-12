66ff5df88b81184b946237c62e3e89b9b2c1f805
kokiebisu <kokiebisu@icloud.com>
2024-01-12 15:26:40 -0700

    fix(web): don't add sentry vite plugin when either running storybook or development server

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,20 +4,25 @@ import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+const plugins = [react()];
+
+if (process.env.npm_lifecycle_event === 'tsc && vite build') {
+  plugins.push(
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    })
+  );
+}
+
 export default defineConfig({
   define: {
     'import.meta.env.PACKAGE_VERSION': JSON.stringify(
       process.env.npm_package_version
     ),
   },
-  plugins: [
-    react(),
-    sentryVitePlugin({
-      org: 'matchya-ai',
-      project: 'web',
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-    }),
-  ],
+  plugins,
   server: {
     host: '127.0.0.1',
     port: 5173,
