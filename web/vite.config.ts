f0f1986fda68f532eba85d8f37515d2e7c7c163f
kokiebisu <kokiebisu@icloud.com>
2024-01-11 21:52:06 -0700

    set up configuration for source map

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,19 +1,31 @@
 import path from 'path';
 
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: 'matchya-vn',
+      project: 'javascript-react',
+    }),
+  ],
+
   server: {
     host: '127.0.0.1',
     port: 5173,
   },
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+
+  build: {
+    sourcemap: true,
   },
 });
