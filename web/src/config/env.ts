d53540b8641337ec020901f141ac3d2924983fb8
kokiebisu <kokiebisu@icloud.com>
2024-01-12 10:51:56 -0700

    fix(web): move away from using deploy_to_s3.sh script and enhancing sentry configuration

--- web/src/config/env.ts
+++ web/src/config/env.ts
@@ -3,4 +3,3 @@ export const clientEndpoint =
 export const apiEndpoint = import.meta.env.VITE_API_URL;
 export const githubClientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
 export const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-export const sentryDsn = import.meta.env.VITE_SENTRY_DSN;
