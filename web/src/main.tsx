c67f3f902d3237d87a81e0404f61d4093811b816
kokiebisu <kokiebisu@icloud.com>
2024-01-11 21:41:32 -0700

    add sentry dsn environment variable

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
+
+import './lib/sentry.ts';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
