3890400ae9c6dd813589e229d09dd9e92e8655ce
kokiebisu <kokiebisu@icloud.com>
2024-01-12 11:35:53 -0700

    fix(ci): sentry source map not uploading

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,27 +4,24 @@ import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     sentryVitePlugin({
-      org: 'matchya-vn',
-      project: 'javascript-react',
+      org: 'matchya',
+      project: 'web',
+      authToken: process.env.SENTRY_AUTH_TOKEN,
     }),
   ],
-
   server: {
     host: '127.0.0.1',
     port: 5173,
   },
-
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-
   build: {
     sourcemap: true,
   },
