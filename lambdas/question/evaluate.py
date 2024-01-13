95ce196dcb7041914421c5207a848e8d640fdd92
takeshi8989 <htakeshi0614@gmail.com>
2024-01-12 16:34:24 -0800

    Refactor audio transcription process

--- lambdas/question/evaluate.py
+++ lambdas/question/evaluate.py
@@ -1,11 +1,8 @@
 import logging
-import json
 import urllib
-import uuid
 
 import boto3
 from openai import OpenAI
-client = OpenAI()
 
 # Logger
 logger = logging.getLogger('evaluate answer')
@@ -25,25 +22,50 @@ db_conn = None
 db_cursor = None
 
 # S3
-s3 = boto3.client('s3')
+s3 = boto3.resource('s3')
+
+# OpenAI
+client = OpenAI()
 
 
-def convert_audio_to_text(audio):
+def get_bucket_name_and_key(event):
     """
-    Converts the audio file to text.
+    Gets the bucket name and key from the event data.
 
-    :param audio: The audio file.
+    :param event: The event data.
     """
-    logger.info("Converting audio to text...")
-    try:
-        transcript = client.audio.transcriptions.create(
-            model="whisper-1",
-            file=audio
-        )
-        logger.info(f"Transcript: {transcript}")
-        return transcript
-    except Exception as e:
-        raise RuntimeError(f'Failed to convert audio to text: {e}')
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    return bucket, key
+
+
+def download_file_from_s3(bucket, key):
+    """
+    Downloads a file from S3.
+
+    :param bucket: The bucket name.
+    :param key: The key.
+    """
+    logger.info(f'Downloading {key} from {bucket}...')
+    local_file_name = '/tmp/' + key
+    s3.download_file(bucket, key, local_file_name)
+    return local_file_name
+
+
+def transcript_from_audio(local_file_name):
+    """
+    Transcribes an audio file.
+
+    :param local_file_name: The local file name.
+    """
+    logger.info(f'Transcribing {local_file_name}...')
+    audio_file = open(local_file_name, "rb")
+    transcript = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file,
+        response_format="text"
+    )
+    return transcript
 
 
 def handler(event, context):
@@ -57,56 +79,14 @@ def handler(event, context):
     try:
         logger.info('Received evaluating an answer request')
 
-        bucket = event['Records'][0]['s3']['bucket']['name']
-        key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-        # test_id = key.split('/')[0]
-        # question_id = key.split('/')[1]
-        # candidate_id = key.split('/')[2].split('.')[0]
+        bucket, key = get_bucket_name_and_key(event)
+        # test_id, question_id, candidate_id = key.split('/')[0], key.split('/')[1], key.split('/')[2].split('.')[0]
 
-        # logger.info(f"Test ID: {test_id}")
-        # logger.info(f"Question ID: {question_id}")
-        # logger.info(f"Candidate ID: {candidate_id}")
+        local_file_name = download_file_from_s3(bucket, key)
+        transcript = transcript_from_audio(local_file_name)
+        logger.info(f'Transcript: {transcript}')
 
-        object_uri = f's3://{bucket}/{key}'
-        transcribe_job_name = 'Test_transcription_' + str(uuid.uuid4())
-
-        transcribe = boto3.client('transcribe')
-        response = transcribe.start_transcription_job(
-            TranscriptionJobName=transcribe_job_name,
-            LanguageCode='en-US',
-            MediaFormat='wav',
-            Media={
-                'MediaFileUri': object_uri
-            }
-        )
-
-        logger.info(f"Transcribe Response: {response}")
-
-        while True:
-            final_response = transcribe.get_transcription_job(TranscriptionJobName=transcribe_job_name)
-            if final_response['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
-                break
-
-        logger.info(f"Final Response: {final_response}")
-
-        transcription_url = final_response["TranscriptionJob"]["Transcript"]["TranscriptFileUri"]
-        transcription_file = urllib.request.urlopen(transcription_url).read().decode("utf-8")
-        transcription = json.loads(transcription_file)["results"]["transcripts"][0]["transcript"]
-
-        logger.info(f"Transcription URL: {transcription_url}")
-        logger.info(f"Transcription File: {transcription_file}")
-        logger.info(f"Transcription: {transcription}")
-        return {
-            'statusCode': 200,
-            'body': json.dumps(f'{transcription}')
-        }
-
-        # response = s3.get_object(Bucket=bucket, Key=key)
-        # audio = response['Body'].read()
-        # text = convert_audio_to_text(audio)
-        # logger.info("Successfully converted audio to text")
-
-        # return
+        return
     except (ValueError, RuntimeError) as e:
         status_code = 400
         logger.error(f'Evaluating an answer failed (status {str(status_code)}): {e}')
