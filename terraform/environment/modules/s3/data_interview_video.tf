565d22ebb23ba5d3daa3f65b2d9a6abeda8db736
kokoaj <kokiebisu@icloud.com>
2024-01-17 10:05:32 -0700

    feat(terraform): create bucket for interview video and audio (#736)

---
+++ terraform/environment/modules/s3/data_interview_video.tf
@@ -0,0 +1,46 @@
+resource "aws_s3_bucket" "data_interview_video" {
+  bucket = "${terraform.workspace}-data-interview-video"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "data_interview_video" {
+  bucket = aws_s3_bucket.data_interview_video.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "POST"]
+    allowed_origins = [var.client_origin]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "data_interview_video" {
+  bucket = aws_s3_bucket.data_interview_video.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "data_interview_video" {
+  bucket = aws_s3_bucket.data_interview_video.id
+
+  depends_on = [aws_s3_bucket_public_access_block.data_interview_video]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource  = "${aws_s3_bucket.data_interview_video.arn}/*"
+      }
+    ]
+  })
+}
