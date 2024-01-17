758867e51a06e5863c08dfceeeb6c890c3b5b9d7
kokoaj <kokiebisu@icloud.com>
2024-01-16 19:31:53 -0700

    feat(web): set up basic page mocked for creating tests (#705)

--- web/src/screens/AssessmentsPage.tsx
+++ web/src/screens/AssessmentsPage.tsx
@@ -1,7 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+
 import AssessmentsPageTemplate from '@/template/AssessmentsPage/AssessmentsPage';
 
 const AssessmentsPage = () => {
-  return <AssessmentsPageTemplate />;
+  const navigate = useNavigate();
+  const handleNavigateToAssessment = () => {
+    navigate('/create-assessment');
+  };
+  return (
+    <AssessmentsPageTemplate
+      onNavigateToAssessment={handleNavigateToAssessment}
+    />
+  );
 };
 
 export default AssessmentsPage;
--- web/src/template/index.ts
+++ web/src/template/index.ts
@@ -5,3 +5,4 @@ export { default as HomePageTemplate } from './HomePage/HomePage';
 export { default as SettingsPageTemplate } from './SettingsPage/SettingsPage';
 export { default as OnboardingPageTemplate } from './OnboardingPage/OnboardingPage';
 export { default as InterviewsPageTemplate } from './InterviewsPage/InterviewsPage';
+export { default as CreateAssessmentPageTemplate } from './CreateAssessmentPage/CreateAssessmentPage';
