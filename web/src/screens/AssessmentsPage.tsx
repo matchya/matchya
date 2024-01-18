0a7bf26fbc816ba71fba35640dbf1b5751873834
takeshi8989 <htakeshi0614@gmail.com>
2024-01-18 15:27:49 -0800

    Add useEffect hook to fetch assessments

--- web/src/screens/AssessmentsPage.tsx
+++ web/src/screens/AssessmentsPage.tsx
@@ -1,9 +1,27 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { axiosInstance } from '@/lib/client';
 import AssessmentsPageTemplate from '@/template/AssessmentsPage/AssessmentsPage';
 
 const AssessmentsPage = () => {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchAssessments();
+  }, []);
+
+  const fetchAssessments = async () => {
+    try {
+      const res = await axiosInstance.get('/assessments');
+      if (res.data.status == 'success') {
+        console.log(res.data.payload);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const handleNavigateToAssessment = () => {
     navigate('/create-assessment');
   };
