fc9d9ccd7fad0772ecf5f99f5fee12fc52e49fa9
kokiebisu <kokiebisu@icloud.com>
2024-01-18 22:37:47 -0700

    set up logic

--- lambdas/video/get_presigned_url.py
+++ lambdas/video/get_presigned_url.py
@@ -1,13 +1,13 @@
 import logging
-
-import psycopg2
+import boto3
+from botocore.exceptions import NoCredentialsError
 
 from config import Config
-from utils.response import generate_success_response, generate_error_response
-from utils.request import parse_header, parse_request_parameter
+from utils.response import generate_error_response, generate_success_response
+from utils.request import parse_header
 
 # Logger
-logger = logging.getLogger('retrieve interview by id')
+logger = logging.getLogger('evaluate interview')
 logger.setLevel(logging.INFO)
 
 formatter = logging.Formatter('[%(levelname)s]:%(funcName)s:%(lineno)d:%(message)s')
@@ -19,109 +19,35 @@ if not logger.handlers:
 
 logger.propagate = False
 
-# Postgres
-db_conn = None
-db_cursor = None
-
-
-def connect_to_db():
-    """
-    Reconnects to the database.
-    """
-    logger.info('Connecting to db...')
-    global db_conn
-    global db_cursor
-    if not db_conn or db_conn.closed:
-        db_conn = psycopg2.connect(host=Config.POSTGRES_HOST, database=Config.POSTGRES_DB, user=Config.POSTGRES_USER, password=Config.POSTGRES_PASSWORD)
-    db_cursor = db_conn.cursor()
-
-
-def retrieve_interview_by_id(interview_id):
-    """
-    Retrieves a interview by interview id from the database.
-
-    :param interview_id: The interview ID.
-    :return: The interview. 
-    """
-    logger.info('Retrieving a interview by interview id from db...')
-    sql = """
-        SELECT 
-            c.id, c.first_name, c.last_name, 
-            a.id, a.name, 
-            i.id, i.total_score, i.summary, i.video_url, i.created_at
-        FROM interview AS i
-        LEFT JOIN candidate AS c ON i.candidate_id = c.id
-        LEFT JOIN assessment AS a ON i.assessment_id = a.id
-        WHERE i.id = '%s'
-    """ % interview_id
-    try:
-        db_cursor.execute(sql)
-        result = db_cursor.fetchall()
-        interview = process_sql_result(result)
-        return interview
-    except Exception as e:
-        raise RuntimeError(f'Failed to retrieve interview by id: {e}')
-
-
-def process_sql_result(result):
-    """
-    Processes the SQL result.
-
-    :param result: The SQL result.
-    :return: The processed result.
-    """
-    interview = {}
-    for row in result:
-        (candidate_id, candidate_first_name, candidate_last_name,
-         assessment_id, assessment_name,
-         interview_id, interview_total_score, interview_summary, interview_video_url, interview_created_at) = row
-        if interview_id not in interview:
-            interview[interview_id] = {
-                'id': interview_id,
-                'total_score': interview_total_score,
-                'summary': interview_summary,
-                'video_url': interview_video_url,
-                'created_at': str(interview_created_at),
-                'candidate': {
-                    'id': candidate_id,
-                    'first_name': candidate_first_name,
-                    'last_name': candidate_last_name
-                },
-                'assessment': {
-                    'id': assessment_id,
-                    'name': assessment_name
-                }
-            }
-    return list(interview.values())
+s3_client = boto3.client('s3')
 
 
 def handler(event, context):
+    """
+    Handles the lambda function call.
+    """
+    logger.info('Generating presigned url...')
+    user_id = 1
+    unique_id = generate_hash()
+    origin = parse_header(event)
     try:
-        logger.info('Retrieving interview by id...')
-        connect_to_db()
-
-        logger.info("Parsing the request header...")
-        origin = parse_header(event)
-
-        logger.info("Parsing the request parameters...")
-        interview_id = parse_request_parameter(event, 'id')
-        interview = retrieve_interview_by_id(interview_id)
-
-        data = {
-            'interviews': interview
-        }
-        logger.info("Successfully retrieved interview by id.")
-        return generate_success_response(origin, data)
-    except (ValueError, RuntimeError) as e:
+        presigned_url = s3_client.generate_presigned_post(
+            Bucket=f'{Config.ENVIRONMENT}-data-question-response-video',
+            Key=f'{user_id}_{unique_id}.webm',
+            ExpiresIn=3600
+        )
+        return generate_success_response(origin, presigned_url)
+    except NoCredentialsError as e:
         status_code = 400
-        logger.error(f'Retrieving interview by id failed: {e}')
         return generate_error_response(origin, status_code, str(e))
-    except Exception as e:
-        status_code = 500
-        logger.error(f'Retrieving interview by id failed: {e}')
-        return generate_error_response(origin, status_code, str(e))
-    finally:
-        if db_cursor:
-            db_cursor.close()
-        if db_conn:
-            db_conn.close()
+
+
+def generate_hash():
+    """
+    Generate a hash to be used as filename
+    """
+    import hashlib
+    import uuid
+    hash_object = hashlib.sha1(uuid.uuid4().bytes)
+    hex_dig = hash_object.hexdigest()
+    return hex_dig
---
+++ lambdas/video/utils/request.py
@@ -0,0 +1,91 @@
+import json
+import io
+import cgi
+
+
+def parse_header(event):
+    """
+    Parses the request header from an event and extracts the origin and host to resolve cors issue
+
+    :param event: The event object containing the request data.
+    :return: origin and the host
+    """
+    try:
+        headers = event['headers']
+        origin = headers.get('origin')
+        if not origin:
+            raise ValueError('Origin not included in headers')
+        return origin
+    except json.JSONDecodeError as e:
+        raise ValueError(f"Invalid JSON in request body: {e}")
+
+
+def parse_request_body(event):
+    """
+    Parses the request body from an event and returns it as a JSON object.
+
+    :param event: The event object containing the request data.
+    :return: Parsed JSON object from the request body.
+    """
+    try:
+        body = event.get('body', '')
+        if not body:
+            raise ValueError("Empty body")
+        return json.loads(body)
+    except json.JSONDecodeError as e:
+        raise ValueError(f"Invalid JSON in request body: {e}")
+
+
+def validate_request_body(body, required_fields):
+    """
+    Validates the necessary fields in the company data.
+
+    :param body: The request body containing company data.
+    """
+    if not all(body.get(field) for field in required_fields):
+        raise ValueError('Missing required fields')
+
+
+def parse_request_parameter(event, parameter_name):
+    """
+    Parses a request parameter from an event and returns it as a string.
+
+    :param event: The event object containing the request data.
+    :param parameter_name: The name of the parameter to parse.
+    :return: The parsed parameter.
+    """
+    try:
+        parameter = event.get('pathParameters', {}).get(parameter_name)
+        if not parameter:
+            raise ValueError(f"Missing required parameter: {parameter_name}")
+        return parameter
+    except Exception as e:
+        raise ValueError(f"Error parsing parameter {parameter_name}: {e}")
+
+
+def parse_cookie_body(event):
+    try:
+        body = event.get('requestContext').get('authorizer')
+        if not body:
+            raise ValueError('Body not included in request')
+        return body
+    except json.JSONDecodeError as e:
+        raise ValueError(f"Invalid JSON in request body: {e}")
+
+
+def parse_form_data_body(event):
+    """
+    Parses the form data from an event and returns it as a JSON object.
+
+    :param event: The event object containing the request data.
+    :return: Parsed JSON object from the request body.
+    """
+    fp = io.BytesIO(event['body'].encode('utf-8'))
+    pdict = cgi.parse_header(event['headers']['Content-Type'])[1]
+    if 'boundary' in pdict:
+        pdict['boundary'] = pdict['boundary'].encode('utf-8')
+    pdict['CONTENT-LENGTH'] = len(event['body'])
+    form_data = cgi.parse_multipart(fp, pdict)
+    for key in form_data.keys():
+        form_data[key] = form_data[key][0]
+    return form_data
