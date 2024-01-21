fce8bdc519176c6e951ae134f1ad676c73333166
kokiebisu <kokiebisu@icloud.com>
2024-01-20 23:05:17 -0700

    update all types to camelcase

--- web/src/types/index.ts
+++ web/src/types/index.ts
@@ -4,23 +4,23 @@ export interface Company {
   id: string;
   name: string;
   email: string;
-  github_username: string;
-  repository_names: string[];
+  githubUsername: string;
+  repositoryNames: string[];
   positions: Position[];
 }
 
 export const candidateSchema = z.object({
   id: z.string(),
-  first_name: z.string(),
-  last_name: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
   email: z.string(),
   assessment: z.object({
-    assessment_id: z.string(),
-    assessment_name: z.string(),
-    interview_id: z.string(),
-    interview_status: z.string(),
-    total_score: z.number(),
-    created_at: z.string(),
+    assessmentId: z.string(),
+    assessmentName: z.string(),
+    interviewId: z.string(),
+    interviewStatus: z.string(),
+    totalScore: z.number(),
+    createdAt: z.string(),
   }),
 });
 
@@ -38,11 +38,11 @@ export type Interview = z.infer<typeof interviewSchema>;
 export const assessmentSchema = z.object({
   id: z.string(),
   name: z.string(),
-  position_type: z.string(),
-  position_level: z.string(),
+  positionType: z.string(),
+  positionLevel: z.string(),
   // createdAt: z.string(),
-  updated_at: z.string(),
-  num_candidates: z.number(),
+  updatedAt: z.string(),
+  numCandidates: z.number(),
 });
 
 export type Assessment = z.infer<typeof assessmentSchema>;
@@ -51,13 +51,13 @@ export interface Criterion {
   id: string;
   message: string;
   keywords: string[];
-  created_at: string;
+  createdAt: string;
 }
 
 export interface Position {
   id: string;
   name: string;
-  checklist_status: string;
+  checklistStatus: string;
   checklist: Checklist;
   candidates: Candidate[];
   questions: Question[];
@@ -65,7 +65,7 @@ export interface Position {
 
 export interface Checklist {
   id: string;
-  repository_names: string[];
+  repositoryNames: string[];
   criteria: Criterion[];
 }
 
