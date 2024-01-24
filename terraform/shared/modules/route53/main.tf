6c49d05f89c5d52742ed5b99a4154661d0bc6e68
kokoaj <kokiebisu@icloud.com>
2024-01-23 18:50:57 -0700

    feat(terraform): set up sendgrid dns (#852)

--- terraform/shared/modules/route53/main.tf
+++ terraform/shared/modules/route53/main.tf
@@ -34,6 +34,54 @@ resource "aws_route53_record" "www_webflow_cname" {
   records = var.www_webflow_records["cname"]
 }
 
+resource "aws_route53_record" "sendgrid_cname_1" {
+  zone_id = aws_route53_zone.main.zone_id
+  name = "${var.sendgrid_cname_1_name}.${var.hosted_zone}"
+  type = "CNAME"
+  ttl = 300
+  records = [var.sendgrid_cname_1_record]
+}
+
+resource "aws_route53_record" "sendgrid_cname_2" {
+  zone_id = aws_route53_zone.main.zone_id
+  name = "${var.sendgrid_cname_2_name}.${var.hosted_zone}"
+  type = "CNAME"
+  ttl = 300
+  records = [var.sendgrid_cname_2_record]
+}
+
+resource "aws_route53_record" "sendgrid_cname_3" {
+  zone_id = aws_route53_zone.main.zone_id
+  name = "${var.sendgrid_cname_3_name}.${var.hosted_zone}"
+  type = "CNAME"
+  ttl = 300
+  records = [var.sendgrid_cname_3_record]
+}
+
+resource "aws_route53_record" "sendgrid_cname_4" {
+  zone_id = aws_route53_zone.main.zone_id
+  name = "${var.sendgrid_cname_4_name}.${var.hosted_zone}"
+  type = "CNAME"
+  ttl = 300
+  records = [var.sendgrid_cname_4_record]
+}
+
+resource "aws_route53_record" "sendgrid_cname_5" {
+  zone_id = aws_route53_zone.main.zone_id
+  name = "${var.sendgrid_cname_5_name}.${var.hosted_zone}"
+  type = "CNAME"
+  ttl = 300
+  records = [var.sendgrid_cname_5_record]
+}
+
+resource "aws_route53_record" "sendgrid_txt" {
+  zone_id = aws_route53_zone.main.zone_id
+  name = "${var.sendgrid_txt_name}.${var.hosted_zone}"
+  type = "TXT"
+  ttl = 300
+  records = [var.sendgrid_txt_record]
+}
+
 data "aws_route53_zone" "main" {
   name = var.hosted_zone
 }
--- terraform/shared/variables.tf
+++ terraform/shared/variables.tf
@@ -17,3 +17,63 @@ variable "www_webflow_records" {
   description = "Map of www webflow records"
   type = map(list(string))
 }
+
+variable "sendgrid_cname_1_name" {
+  description = "The name of the first sendgrid cname record"
+  type = string
+}
+
+variable "sendgrid_cname_1_record" {
+  description = "The record of the first sendgrid cname record"
+  type = string
+}
+
+variable "sendgrid_cname_2_name" {
+  description = "The name of the second sendgrid cname record"
+  type = string
+}
+
+variable "sendgrid_cname_2_record" {
+  description = "The record of the second sendgrid cname record"
+  type = string
+}
+
+variable "sendgrid_cname_3_name" {
+  description = "The name of the third sendgrid cname record"
+  type = string
+}
+
+variable "sendgrid_cname_3_record" {
+  description = "The record of the third sendgrid cname record"
+  type = string
+}
+
+variable "sendgrid_cname_4_name" {
+  description = "The name of the fourth sendgrid cname record"
+  type = string
+}
+
+variable "sendgrid_cname_4_record" {
+  description = "The record of the fourth sendgrid cname record"
+  type = string
+}
+
+variable "sendgrid_cname_5_name" {
+  description = "The name of the fifth sendgrid cname record"
+  type = string
+}
+
+variable "sendgrid_cname_5_record" {
+  description = "The record of the fifth sendgrid cname record"
+  type = string
+}
+
+variable "sendgrid_txt_name" {
+  description = "The sendgrid txt name"
+  type = string
+}
+
+variable "sendgrid_txt_record" {
+  description = "The sendgrid txt record"
+  type = string
+}
