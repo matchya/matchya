dc02cbde00eda77b7f7433c9510ab4c754c56e2d
kokiebisu <kokiebisu@icloud.com>
2024-01-23 18:48:58 -0700

    hook up module

--- terraform/shared/main.tf
+++ terraform/shared/main.tf
@@ -11,6 +11,18 @@ module "route53" {
   google_workspace_records = var.google_workspace_records
   webflow_records = var.webflow_records
   www_webflow_records = var.www_webflow_records
+  sendgrid_cname_1_name = var.sendgrid_cname_1_name
+  sendgrid_cname_1_record = var.sendgrid_cname_1_record
+  sendgrid_cname_2_name = var.sendgrid_cname_2_name
+  sendgrid_cname_2_record = var.sendgrid_cname_2_record
+  sendgrid_cname_3_name = var.sendgrid_cname_3_name
+  sendgrid_cname_3_record = var.sendgrid_cname_3_record
+  sendgrid_cname_4_name = var.sendgrid_cname_4_name
+  sendgrid_cname_4_record = var.sendgrid_cname_4_record
+  sendgrid_cname_5_name = var.sendgrid_cname_5_name
+  sendgrid_cname_5_record = var.sendgrid_cname_5_record
+  sendgrid_txt_name = var.sendgrid_txt_name
+  sendgrid_txt_record = var.sendgrid_txt_record
 }
 
 module "ses" {
