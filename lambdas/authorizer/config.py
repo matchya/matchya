d092c047448ec6717787949a157f375a04968e31
kokiebisu <kokiebisu@icloud.com>
2024-01-27 14:08:48 -0700

    refactor(authorizer): split code into client, utils

--- lambdas/authorizer/config.py
+++ lambdas/authorizer/config.py
@@ -8,6 +8,7 @@ class Config:
     ENVIRONMENT = os.getenv('ENVIRONMENT', 'dev')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SENTRY_DSN = os.getenv('SENTRY_DSN')
+    SERVICE_NAME = os.getenv('SERVICE_NAME')
 
     @classmethod
     def validate(cls):
@@ -15,7 +16,7 @@ class Config:
         This method checks if all necessary configuration variables are set.
         It raises an exception if any required configuration is missing.
         """
-        required_variables = ['JWT_SECRET_KEY']
+        required_variables = ['JWT_SECRET_KEY', 'SERVICE_NAME']
         missing_variables = [variable for variable in required_variables if not getattr(cls, variable)]
 
         if missing_variables:
