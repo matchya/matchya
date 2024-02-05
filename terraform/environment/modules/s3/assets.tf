b95456a84579a10409c526a8a5ba467d502b01d1
kokiebisu <kokiebisu@icloud.com>
2024-02-04 21:02:05 -0700

    refactor(terraform): rename asset bucket to 'matchya-assets

---
+++ terraform/environment/modules/s3/assets.tf
@@ -0,0 +1,46 @@
+resource "aws_s3_bucket" "assets" {
+  bucket = "${var.namespace}-assets"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "assets" {
+  bucket = aws_s3_bucket.assets.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "assets" {
+  bucket = aws_s3_bucket.assets.id
+  depends_on = [ aws_s3_bucket.assets, aws_s3_bucket_public_access_block.assets ]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "DenyInsecureTransport"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:PutObject"
+        Resource  = "${aws_s3_bucket.assets.arn}/*"
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      },
+      {
+        Sid       = "AllowPublicRead"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.assets.arn}/*"
+      }
+    ]
+  })
+}
