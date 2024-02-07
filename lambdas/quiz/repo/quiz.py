fd0053fee0c863a8e182bbf3b95040e375d467cc
takeshi8989 <htakeshi0614@gmail.com>
2024-02-07 01:15:31 -0800

    Add Dockerfile and serverless configuration files***
    ***Add logger and package_info modules***
    
    ***Add sentry client module***
    
    ***Add interview entity module***
    
    ***Add .dockerignore and changelog.config.js files

---
+++ lambdas/quiz/repo/quiz.py
@@ -0,0 +1,40 @@
+import os
+from typing import List
+
+from client.postgres import PostgresDBClient
+from entity.quiz import Quiz
+from utils.logger import Logger
+
+logger = Logger.configure(os.path.relpath(__file__, os.path.join(os.path.dirname(__file__), '..')))
+
+
+class QuizRepository:
+    """
+    This class is responsible for all the database operations
+    """
+
+    def __init__(self, db_client: PostgresDBClient):
+        self.db_client = db_client
+
+    def retrieve_many(self) -> List[Quiz]:
+        """
+        Retrieves quizes from the database.
+
+        :return: The quizes.
+        """
+        logger.info('Retrieving quizes')
+        sql = "SELECT id, description, topic, subtopic, difficulty, created_at FROM quiz LIMIT 10;"
+        self.db_client.execute(sql)
+        result = self.db_client.fetchall()
+        quizes = []
+        for row in result:
+            quiz = Quiz()
+            quiz.id = row[0]
+            quiz.description = row[1]
+            quiz.topic = row[2]
+            quiz.subtopic = row[3]
+            quiz.difficulty = row[4]
+            quiz.created_at = str(row[5])
+            quizes.append(quiz)
+        logger.info('Successfully retrieved questions')
+        return quizes
---
+++ lambdas/quiz/retrieve.py
@@ -0,0 +1,42 @@
+import os
+
+from client.postgres import PostgresDBClient
+from client.sentry import SentryClient
+from repo.quiz import QuizRepository
+from utils.logger import Logger
+from utils.package_info import PackageInfo
+from utils.request_parser import RequestParser
+from utils.response_generator import ResponseGenerator
+
+logger = Logger.configure(os.path.relpath(__file__, os.path.join(os.path.dirname(__file__), '.')))
+
+SentryClient.initialize(PackageInfo('package.json').get_version())
+postgres_client = PostgresDBClient()
+response_generator = ResponseGenerator()
+
+
+# TODO: This lambda must handle searching for keywords and filtering based on position info
+def handler(event, context):
+    logger.info('Starting lambda execution')
+    try:
+        # initialize the parser
+        parser = RequestParser(event)
+
+        # parsing from the event
+        origin = parser.parse_header()
+        response_generator.origin_domain = origin
+
+        # business logic
+        with postgres_client as db_client:
+            quiz_repo = QuizRepository(db_client)
+            quizes = quiz_repo.retrieve_many()
+
+        return response_generator.generate_success_response({
+            'quizes': [quiz.to_dict() for quiz in quizes]
+        })
+    except (ValueError, RuntimeError) as e:
+        logger.error(f'Retrieving quizes failed: {e}')
+        return response_generator.generate_error_response(400, str(e))
+    except Exception as e:
+        logger.error(f'Retrieving quizes failed: {e}')
+        return response_generator.generate_error_response(500, str(e))
