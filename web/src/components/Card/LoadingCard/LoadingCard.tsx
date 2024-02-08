cb4cfe2e00025b6bb6362682f106fd1ac298308f
kokiebisu <kokiebisu@icloud.com>
2024-02-08 16:50:02 -0700

    style(web): loading card skeleton

--- web/src/components/Card/LoadingCard/LoadingCard.tsx
+++ web/src/components/Card/LoadingCard/LoadingCard.tsx
@@ -14,13 +14,12 @@ const LoadingCard = () => {
         <div className="space-y-1">
           <div className="flex items-center space-x-3 mb-3">
             <CardTitle className="text-sm text-muted-foreground">
-              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="rounded-full h-3 w-[50px]" />
             </CardTitle>
           </div>
           <CardDescription className="text-md text-black space-y-3">
-            <Skeleton className="h-4 w-[600px]" />
-            <Skeleton className="h-4 w-[500px]" />
-            <Skeleton className="h-4 w-[400px]" />
+            <Skeleton className="rounded-full h-4 w-3/4" />
+            <Skeleton className="rounded-full h-4 w-1/2" />
           </CardDescription>
         </div>
       </CardHeader>
