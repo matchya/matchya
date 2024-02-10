9f79e723b50b6ffed0c58ceb1f00bfb7109dca85
kokiebisu <kokiebisu@icloud.com>
2024-02-10 13:59:52 -0700

    style(web): clean up interview detail page

--- web/src/template/InterviewDetailPage/InterviewDetailPage.tsx
+++ web/src/template/InterviewDetailPage/InterviewDetailPage.tsx
@@ -24,116 +24,108 @@ const InterviewDetailPageTemplate = ({
   return (
     <div className="h-[calc(100vh-64px)] overflow-y-scroll">
       <div className="w-full mx-auto">
-        <div className="w-full mx-auto">
-          <div className="justify-between items-center pt-6">
-            <div className="px-4 md:px-12">
-              <div className="w-full flex">
-                <div className="w-2/3">
+        <div className="justify-between items-center py-12">
+          <div className="px-4 md:px-12">
+            <div className="w-full space-y-8">
+              <div className="grid lg:grid-cols-2 lg:mb-0">
+                <div className="items-center lg:pr-4 lg:relative lg:top-[5px] mb-6 lg:mb-0">
                   <div className="mb-4 flex justify-between items-center">
                     <div className="space-y-4">
-                      <Link
-                        to="/interviews"
-                        className="text-lg font-bold text-matcha-700"
-                      >
-                        ← Back to Interviews
+                      <Link to="/interviews">
+                        <div className="w-full flex justify-start">
+                          <p className="text-xl font-bold text-matcha-800">
+                            ← Back
+                          </p>
+                        </div>
                       </Link>
                     </div>
                   </div>
-                  <div className="w-full ml-3 flex justify-around mb-3 items-center">
-                    <p className="text-3xl font-bold">
-                      {interviewData.candidate.name}
-                    </p>
-                    <p className="text-2xl">
-                      {interviewData.assessment.name} -{' '}
-                      {interviewData.createdAt.substring(0, 10)}
-                    </p>
+                  <p className="text-3xl font-bold">
+                    {interviewData.candidate.name}
+                  </p>
+                  <p className="text-md text-gray-600 mb-8">
+                    {interviewData.assessment.name} -{' '}
+                    {interviewData.createdAt.substring(0, 10)}
+                  </p>
+                  <div className="lg:flex-grow-0 lg:flex-shrink-0">
+                    <ReactPlayer
+                      url={currentAnswer?.videoUrl}
+                      controls={true}
+                      width={'100%'}
+                      height={'auto'}
+                    />
                   </div>
-                  <div className="w-full flex flex-col items-center justify-center">
-                    <div className="lg:flex-grow-0 lg:flex-shrink-0">
-                      <ReactPlayer
-                        url={currentAnswer?.videoUrl}
-                        controls={true}
-                        className="w-full h-full max-h-[1087px] object-contain"
-                      />
+                </div>
+                <div className="lg:pl-4 w-full">
+                  <div className="overflow-hidden w-full rounded border border-gray-200">
+                    <div className="p-3">
+                      <p className="text-2xl font-bold">Questions</p>
                     </div>
-                    <div className="w-4/5 mt-8 bg-white border p-10 mx-20 rounded">
-                      <div className="flex px-10 justify-around">
-                        <h3 className="text-2xl font-bold text-matcha-500">
-                          Review By Matchya AI
-                        </h3>
-                        <p className="text-lg">
-                          Total Score:{' '}
-                          <span className="text-2xl font-bold text-macha-700 ml-1">
-                            {interviewData.totalScore}
-                          </span>{' '}
-                          %
-                        </p>
-                      </div>
-                      <p className="text-lg pt-3 pl-3 ml-3">
-                        {interviewData.summary}
-                      </p>
+                    <div className="max-h-[150px] lg:max-h-none overflow-y-scroll">
+                      {interviewData.answers.map(answer => (
+                        <div
+                          className={`w-full flex cursor-pointer hover:bg-orange-100 p-3`}
+                          key={answer.quiz.id}
+                          onClick={() => handleSelectVideo(answer)}
+                        >
+                          <ReactPlayer
+                            url={answer.videoUrl}
+                            controls={false}
+                            playing={false}
+                            width={320}
+                            height={'100%'}
+                          />
+                          <div className="w-full p-2">
+                            <p
+                              className={`text-md font-bold ${
+                                currentAnswer?.quiz.id === answer.quiz.id &&
+                                'text-matcha-500'
+                              }`}
+                            >
+                              {answer.quiz.topic}
+                            </p>
+                            <p className="text-xs">
+                              {answer.quiz.description + '...'}
+                            </p>
+                          </div>
+                        </div>
+                      ))}
                     </div>
                   </div>
                 </div>
-                <div className="w-1/3 pl-6 pt-3">
-                  <div className="max-h-[60vh] overflow-y-scroll bg-white border rounded-lg mb-6 pb-4">
-                    <p className="text-2xl pt-3 pl-3 font-bold ml-3 my-3">
-                      Questions
+              </div>
+              <div className="grid lg:grid-cols-2">
+                <div className="space-y-2 pr-4 lg:ml-0 mb-8 lg:mb-0">
+                  <p className="text-lg font-bold">Question</p>
+                  <div className="flex justify-between items-center mb-2">
+                    <h3 className="text-matcha-500">
+                      {currentAnswer?.quiz.topic}
+                    </h3>
+                    <p className="text-md">
+                      Score:{' '}
+                      <span className="text-matcha-700">
+                        {currentAnswer?.score}
+                      </span>{' '}
+                      %
                     </p>
-
-                    {interviewData.answers.map(answer => (
-                      <div
-                        className={`w-full flex items-center cursor-pointer hover:bg-orange-100 my-4 px-2`}
-                        key={answer.quiz.id}
-                        onClick={() => handleSelectVideo(answer)}
-                      >
-                        <ReactPlayer
-                          url={answer.videoUrl}
-                          controls={false}
-                          playing={false}
-                          width={200}
-                          height={'auto'}
-                        />
-                        <div className="w-2/3 p-2">
-                          <p
-                            className={`text-xl font-bold ${
-                              currentAnswer?.quiz.id === answer.quiz.id &&
-                              'text-matcha-500'
-                            }`}
-                          >
-                            {answer.quiz.topic}
-                          </p>
-                          <p className="text-xs">
-                            {answer.quiz.description + '...'}
-                          </p>
-                        </div>
-                      </div>
-                    ))}
                   </div>
-                  <div className="p-6 bg-white border rounded-lg mb-12">
-                    <p className="text-2xl text-center font-bold text-black mb-4">
-                      Question
+                  <p className="text-sm mb-6">
+                    {currentAnswer?.quiz.description}
+                  </p>
+                  <p className="text-sm">{currentAnswer?.feedback}</p>
+                </div>
+                <div className="bg-white rounded space-y-3 lg:pl-4">
+                  <div className="space-y-1">
+                    <h3 className="text-matcha-500">Review By Matchya AI</h3>
+                    <p className="text-xl font-bold">
+                      Total Score:{' '}
+                      <span className="text-macha-700">
+                        {interviewData.totalScore}
+                      </span>{' '}
+                      %
                     </p>
-                    <div className="flex justify-around mb-2">
-                      <h3 className="text-xl font-bold text-matcha-500">
-                        {currentAnswer?.quiz.topic}
-                      </h3>
-                      <p className="text-md">
-                        Score:{' '}
-                        <span className="text-xl font-bold text-matcha-700 ml-1">
-                          {currentAnswer?.score}
-                        </span>{' '}
-                        %
-                      </p>
-                    </div>
-                    <p className="indent-4 text-sm mb-6">
-                      {currentAnswer?.quiz.description}
-                    </p>
-                    <p className="text-2xl text-center font-bold text-black mb-4">
-                      Feedback
-                    </p>
-                    <p className="text-sm">{currentAnswer?.feedback}</p>
                   </div>
+                  <p className="text-md">{interviewData.summary}</p>
                 </div>
               </div>
             </div>
