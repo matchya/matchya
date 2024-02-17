c1d5d3cd5d3f4f34457e1be6b33419784e95c17c
kokiebisu <kokiebisu@icloud.com>
2024-02-16 22:08:22 -0700

    initial commit

--- terraform/shared/modules/iam/user_github_actions.tf
+++ terraform/shared/modules/iam/user_github_actions.tf
@@ -14,7 +14,11 @@ resource "aws_iam_policy" "github_actions" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:*",
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession",
+          "iam:PassRole"
+        ],
         Effect = "Allow",
         Resource = aws_iam_role.github_actions_user_role.arn
       },
