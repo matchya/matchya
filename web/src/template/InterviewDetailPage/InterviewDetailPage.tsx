e77a88c9de3a2cf91841c420470a4d36c2815c87
Thomas Bui <thomasbui198@gmail.com>
2024-02-18 01:03:01 -0800

    Add tabs and basic text for interview detail

--- web/src/template/InterviewDetailPage/InterviewDetailPage.tsx
+++ web/src/template/InterviewDetailPage/InterviewDetailPage.tsx
@@ -2,6 +2,12 @@ import { useState } from 'react';
 import ReactPlayer from 'react-player';
 import { Link } from 'react-router-dom';
 
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@/components/Tabs/Tabs';
 import { trackEvent } from '@/lib/rudderstack';
 import { Answer, Interview } from '@/types';
 
@@ -23,114 +29,52 @@ const InterviewDetailPageTemplate = ({
   };
   return (
     <div className="h-[calc(100vh-64px)] overflow-y-scroll">
-      <div className="w-full mx-auto">
-        <div className="justify-between items-center py-12">
-          <div className="px-4 md:px-12">
-            <div className="w-full space-y-8">
-              <div className="lg:flex lg:mb-0">
-                <div className="items-center lg:pr-4 lg:relative lg:top-[5px] mb-6 lg:mb-0">
-                  <div className="mb-4 flex justify-between items-center">
-                    <div className="space-y-4">
-                      <Link to="/interviews">
-                        <div className="w-full flex justify-start">
-                          <p className="text-xl font-bold text-matcha-800">
-                            ← Back
-                          </p>
-                        </div>
-                      </Link>
-                    </div>
-                  </div>
-                  <p className="text-3xl font-bold">
-                    {interviewData.candidate.name}
-                  </p>
-                  <p className="text-md text-gray-600 mb-8">
-                    {interviewData.assessment.name} -{' '}
-                    {interviewData.createdAt.substring(0, 10)}
-                  </p>
-                  <div className="lg:flex-grow-0 lg:flex-shrink-0 mb-4">
-                    <ReactPlayer
-                      url={currentAnswer?.videoUrl}
-                      controls={true}
-                      width={'100%'}
-                      height={'auto'}
-                    />
-                  </div>
-                  <div className="space-y-2 pr-4 lg:ml-0 mb-8 lg:mb-0">
-                    <p className="text-lg font-bold">Question</p>
-                    <div className="flex justify-between items-center mb-2">
-                      <h3 className="text-matcha-500">
-                        {currentAnswer?.quiz.topic}
-                      </h3>
-                      <p className="text-md">
-                        Score:{' '}
-                        <span className="text-matcha-700">
-                          {currentAnswer?.score}
-                        </span>{' '}
-                        %
-                      </p>
-                    </div>
-                    <p className="text-sm mb-6">
-                      {currentAnswer?.quiz.description}
-                    </p>
-                    <p className="text-sm">{currentAnswer?.feedback}</p>
-                  </div>
-                </div>
-                <div className="lg:pl-4 w-full">
-                  <div className="overflow-hidden w-full rounded-lg border border-gray-200">
-                    <div className="p-3 border-b border-gray-200">
-                      <p className="text-2xl font-bold">Questions</p>
-                    </div>
-                    <div className=" h-full max-h-[300px] lg:max-h-[600px] overflow-y-scroll">
-                      {interviewData.answers.map(answer => (
-                        <div
-                          className={`w-full flex cursor-pointer hover:bg-orange-100 p-3`}
-                          key={answer.quiz.id}
-                          onClick={() => handleSelectVideo(answer)}
-                        >
-                          <ReactPlayer
-                            url={answer.videoUrl}
-                            controls={false}
-                            playing={false}
-                            width={320}
-                            height={'100%'}
-                          />
-                          <div className="w-full p-2">
-                            <p
-                              className={`text-md font-bold ${
-                                currentAnswer?.quiz.id === answer.quiz.id &&
-                                'text-matcha-500'
-                              }`}
-                            >
-                              {answer.quiz.topic}
-                            </p>
-                            <p className="text-xs">
-                              {answer.quiz.description + '...'}
-                            </p>
-                          </div>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                </div>
+      <div className="flex flex-col items-center mb-8">
+        <p className="text-3xl font-bold">{interviewData.candidate.name}</p>
+        <p className="text-md ">{interviewData.candidate.email}</p>
+        <p className="text-md text-gray-600">
+          {interviewData.assessment.name} -{' '}
+          {interviewData.createdAt.substring(0, 10)}
+        </p>
+      </div>
+      <div className="mb-8">
+        <p className="text-xl font-bold text-center">
+          Overall feedback from Matchya AI
+        </p>
+        <p>{interviewData.summary}</p>
+      </div>
+
+      <Tabs defaultValue={interviewData.answers[0].quiz.id} className="mb-8">
+        <TabsList>
+          {interviewData.answers.map(answer => (
+            <TabsTrigger value={answer.quiz.id}>
+              {answer.quiz.subtopic}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        {interviewData.answers.map(answer => (
+          <TabsContent value={answer.quiz.id}>
+            <div>
+              <div className="flex flex-row mb-4">
+                <div>Graph</div>
+                <div>Card</div>
               </div>
-              <div className="bg-white rounded space-y-3">
-                <div className="space-y-1">
-                  <h3 className="text-matcha-500 text-2xl font-bold">
-                    Review By Matchya AI
-                  </h3>
-                  <p className="text-xl">
-                    Total Score:{' '}
-                    <span className="text-macha-700">
-                      {interviewData.totalScore}
-                    </span>{' '}
-                    %
-                  </p>
-                </div>
-                <p className="text-md">{interviewData.summary}</p>
+              <div>
+                <p className="text-xl font-bold">Answer Summary & Feedback</p>
+                <p>{answer.feedback}</p>
               </div>
             </div>
-          </div>
-        </div>
+          </TabsContent>
+        ))}
+      </Tabs>
+
+      <div className="lg:flex-grow-0 lg:flex-shrink-0 mb-4">
+        <ReactPlayer
+          url={currentAnswer?.videoUrl}
+          controls={true}
+          width={'50%'}
+          height={'auto'}
+        />
       </div>
     </div>
   );
